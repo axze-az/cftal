9c04a096c6a125edb5ea9d38ae13a4f70dce7b96
axel <axel@18305beb-b1e2-0310-bcd8-b14b5419b1d7>
2012-07-07 16:23:50 +0000

    heap_array internal functions
    git-svn-id: file:///usr/local/svn/cpplib/trunk/cftal@326 18305beb-b1e2-0310-bcd8-b14b5419b1d7

--- allocator.h
+++ allocator.h
@@ -25,47 +25,71 @@ namespace cftal {
 		static std::size_t max_blocks();
 	};
 
-	template <class _T, std::size_t _N1, std::size_t _N2=_N1>
-	class allocator : public std::allocator<_T> {
-		static_assert((_N1 % sizeof(_T)) ==0, "_N1 invalid");
-		static_assert((_N2 % sizeof(_T)) ==0, "_N2 invalid");
+	template <class _T, std::size_t _N>
+	class cache_allocator : public std::allocator<_T> {
+		static_assert((N==0) || ((_N % sizeof(_T)) ==0), 
+			      "_N should be an multiple of _T");
 		typedef std::allocator<_T> base_type;
 	public:
-		allocator() : base_type() {}
-		allocator(const allocator& r) : base_type(r) {};
-		~allocator() {}
-
+		cache_allocator() : base_type() {}
+		cache_allocator(const cache_allocator& r) : base_type(r) {};
+		~cache_allocator() {}
 		template<typename _Tp1>
-		struct rebind { typedef allocator<_Tp1> other; };
-
+		struct rebind { typedef cache_allocator<_Tp1> other; };
 		pointer allocate(size_type __n, const void* __p= nullptr) { 
 			size_type s= __n * sizeof(_T); 
 			if (s==_N1) {
-				void* r= ptr_cache<_N1>::get();
+				void* r= ptr_cache<_N>::get();
 				if (r != nullptr)
 					return static_cast<pointer>(r);
 			}
-			if (_N1 != _N2) {
-				if (s==_N2) {
-					void* r= ptr_cache<_N1>::get();
-					if (r != nullptr)
-						return static_cast<pointer>(r);
-				}
-			}
 			return base_type::allocate(__n, __p);
 		}
-
 		void deallocate(pointer __p, size_type __n) { 
 			size_type s= __n* sizeof(_T);
-			if (s==_N1 && ptr_cache<_N1>::put(__p)==nullptr)
+			if (s==_N1 && ptr_cache<_N>::put(__p)==nullptr)
 				return;
-			if (_N1 != _N2) {
-				if (s==_N2 && ptr_cache<_N1>::put(__p)==nullptr)
-					return;
-			}
 			base_type::deallocate(__p, __n);
 		}
 	};
+
+	template <class _T, std::size_t _N,
+		  class _A = std::allocator<_T> >
+	class heap_array : private _A {
+		_T* _v;
+		_T* alloc(_A& a) {
+			_T* p = a.allocate(_N);
+			std::size_t i;
+			try {
+				for (i=0; i<_N; ++i)
+					a.construct(p+i, _T());
+			}
+			catch (...) {
+				for (std::size_t j=0; j<i; ++j)
+					a.destroy(p+j);
+				throw;
+			}
+		}
+		_T* alloc_and_copy(A& a, const _T* rp) {
+			_T* p = a.allocate(_N);
+			std::size_t i;
+			try {
+				for (i=0; i<_N; ++i)
+					a.construct(p+i, rp[i]);
+			}
+			catch (...) {
+				for (std::size_t j=0; j<i; ++j)
+					a.destroy(p+j);
+				throw;
+			}
+		}
+		void destroy(A& a, _T* p) {
+			for (std::size_t j=0; j<_N; ++j)
+				a.destroy(p+j);
+		}
+	public:
+	};
+
 }
 
 
