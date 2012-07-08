f5dd77345d9b99a426f86fc501e6a17d28cf5f15
axel <axel@18305beb-b1e2-0310-bcd8-b14b5419b1d7>
2012-07-08 14:53:21 +0000

    more updates
    git-svn-id: file:///usr/local/svn/cpplib/trunk/cftal@333 18305beb-b1e2-0310-bcd8-b14b5419b1d7

--- emuvec.h
+++ emuvec.h
@@ -1,5 +1,24 @@
 #if !defined (__EMUVEC_H__)
 #define __EMUVEC_H__ 1
 
+#include <cftal/heap_array.h>
+#include <cftal/allocator.h>
 
+namespace emuvec {
+
+	template <typename _T, std::size_t _N>
+	class vxy {
+		typedef
+		cftal::heap_array<_T, _N, cftal::cache_allocator<_T, _N> >
+		vec_t;
+		vec_t _v;
+	public:
+		template <typename _T, std::size_t>
+		friend class ops;
+	};
+
+}
+// Local variables:
+// mode: c++
+// end:
 #endif
