b5aff7fd427c680b2459344586c3aff3ac9d95af
axel <axel@18305beb-b1e2-0310-bcd8-b14b5419b1d7>
2012-07-16 16:31:48 +0000

    more updates
    git-svn-id: file:///usr/local/svn/cpplib/trunk/cftal@359 18305beb-b1e2-0310-bcd8-b14b5419b1d7

---
+++ x86vec_divisor.h
@@ -0,0 +1,31 @@
+#if !defined (__X86VEC_DIVISOR_H__)
+#define __X86VEC_DIVISOR_H__ 1
+
+#include <cftal/x86vec_ivec.h>
+#include <cftal/x86vec_fvec.h>
+#include <cftal/divisor.h>
+
+namespace x86vec {
+
+	namespace impl {
+		struct vdiv16_traits {
+		};
+
+		struct vdiv32_traits {
+		};
+	}
+}
+
+namespace cftal {
+
+}
+
+namespace x86vec {
+
+}
+
+
+// Local variables:
+// mode: c++
+// end:
+#endif
