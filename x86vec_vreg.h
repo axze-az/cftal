323f06dabac7c9a9df8a5fe467e7203f3f5d2a91
axel <axel@18305beb-b1e2-0310-bcd8-b14b5419b1d7>
2012-10-04 12:42:39 +0000

    masked_vec
    git-svn-id: file:///usr/local/svn/cpplib/trunk/cftal@508 18305beb-b1e2-0310-bcd8-b14b5419b1d7

--- x86vec_vreg.h
+++ x86vec_vreg.h
@@ -29,15 +29,13 @@ namespace x86vec {
 	// mask of a vector.
 	template <class _V>
 	class mask {
+		_V _v;
 	public:
 		typedef typename _V::vector_type vector_type;
 		mask() = default;
-		mask(const _V& x);
-		mask(vector_type x);
 		mask& operator=(const mask& r) = default;
-		vector_type operator()() const;
-	private:
-		vector_type _v;
+		mask(const _V& x);
+		_V operator()() const;
 	};
 
 	// masked vector to allow something like v x;
@@ -81,16 +79,26 @@ x86vec::mask<_V>::mask(const _V& x) : _v(x())
 }
 
 template <class _V>
+inline 
+_V x86vec::mask<_V>::operator()() const 
+{
+	return _v;
+}
+
+template <class _V>
 inline
-x86vec::mask<_V>::mask(vector_type v) : _v(v)
+x86vec::masked_vec<_V>::masked_vec(_V& v, const mask<_V>& m)
+	: _v(&v), _m(m)
 {
 }
 
 template <class _V>
-typename x86vec::mask<_V>::vector_type
-x86vec::mask<_V>::operator()() const 
+inline
+_V& x86vec::masked_vec<_V>::operator=(const _V& r)
 {
-	return _v;
+	_V& res= *_v;
+	res = select(_m(), r, res);
+	return res;
 }
 
 // Local variables:
