e823d995e49e3a4a305908de5acf87b0cd958237
axel <axel@18305beb-b1e2-0310-bcd8-b14b5419b1d7>
2013-01-19 10:35:11 +0000

    use more macros
    git-svn-id: file:///usr/local/svn/cpplib/trunk/cftal@542 18305beb-b1e2-0310-bcd8-b14b5419b1d7

--- x86vec_expr.h
+++ x86vec_expr.h
@@ -108,199 +108,60 @@ namespace x86vec {
 				       eval(e._r));
 	}
 
-#define DEFINE_X86VEC_FP_OPERATORS(vx)					\
-	/* operator +(V, V) */						\
-	inline								\
-	expr<ops::add<vx>, vx, vx>					\
-	operator+(const vx& a, const vx& b) {				\
-		return expr<ops::add<vx>, vx, vx>(a,b);			\
+#define DEFINE_X86VEC_FP_OPERATOR(vx, op, eq_op, op_name)		\
+        /* operator op(V, V) */                                         \
+        inline                                                          \
+        expr<ops:: op_name<vx>, vx, vx>                                 \
+        operator op (const vx& a, const vx& b) {			\
+                return expr<ops:: op_name<vx>, vx, vx>(a,b);		\
+        }                                                               \
+        /* operator op(V, expr) */                                      \
+        template <template <class _V> class _OP, class _L, class _R>    \
+        inline                                                          \
+        expr<ops:: op_name<vx>, vx, expr<_OP<vx>, _L, _R> >             \
+        operator op (const vx& a, const expr<_OP<vx>, _L, _R>& b) {	\
+                return expr<ops:: op_name<vx>,				\
+                            vx, expr<_OP<vx>, _L, _R> >(a, b);          \
 	}								\
-	/* operator +(V, expr) */					\
-	template <template <class _V> class _OP, class _L, class _R>	\
-	inline								\
-	expr<ops::add<vx>, vx, expr<_OP<vx>, _L, _R> >			\
-	operator+(const vx& a, const expr<_OP<vx>, _L, _R>& b) {	\
-		return expr<ops::add<vx>,				\
-			    vx, expr<_OP<vx>, _L, _R> >(a, b);		\
+        /* operator op(expr, V) */                                      \
+        template <template <class _V> class _OP, class _L, class _R>    \
+        inline                                                          \
+        expr<ops:: op_name<vx>, expr<_OP<vx>, _L, _R>, vx>              \
+        operator op (const expr<_OP<vx>, _L, _R>& a, const vx& b) {	\
+                return expr<ops:: op_name<vx>,				\
+                            expr<_OP<vx>, _L, _R>, vx>(a, b);           \
 	}								\
-	/* operator +(expr, V) */					\
-	template <template <class _V> class _OP, class _L, class _R>	\
-	inline								\
-	expr<ops::add<vx>, expr<_OP<vx>, _L, _R>, vx>			\
-	operator+(const expr<_OP<vx>, _L, _R>& a, const vx& b) {	\
-		return expr<ops::add<vx>,				\
-			    expr<_OP<vx>, _L, _R>, vx>(a, b);		\
-	}								\
-	/* operator +(expr, expr)  */					\
+	/* operator op(expr, expr)  */					\
 	template <template <class _V> class _OP1, class _L1, class _R1,	\
 		  template <class _V> class _OP2, class _L2, class _R2>	\
 	inline								\
-	expr<ops::add<vx>,						\
+	expr<ops:: op_name<vx>,						\
 	     expr<_OP1<vx>, _L1, _R1>, expr<_OP2<vx>, _L2, _R2> >	\
-	operator+(const expr<_OP1<vx>, _L1, _R1>& a,			\
-		  const expr<_OP2<vx>, _L2, _R2>& b) {			\
-		return expr<ops::add<vx>,				\
+	operator op(const expr<_OP1<vx>, _L1, _R1>& a,			\
+		    const expr<_OP2<vx>, _L2, _R2>& b) {		\
+		return expr<ops:: op_name<vx>,				\
 			    expr<_OP1<vx>, _L1, _R1>,			\
 			    expr<_OP2<vx>, _L2, _R2> > (a, b);		\
 	}								\
-	/* operator += V */						\
-	inline								\
-	vx& operator+=(vx& a, const vx& r) {				\
-		a = a + r;						\
-		return a;						\
-	}								\
-	/* operator += expr */						\
-	template <template <class _V> class _OP, class _L, class _R>	\
-	inline								\
-	vx& operator+=(vx& a, const expr<_OP<vx>, _L, _R>& r) {		\
-		a = a + r;						\
-		return a;						\
-	}								\
-									\
-	/* operator -(V, V) */						\
-	inline								\
-	expr<ops::sub<vx>, vx, vx>					\
-	operator-(const vx& a, const vx& b) {				\
-		return expr<ops::sub<vx>, vx, vx>(a,b);			\
-	}								\
-	/* operator -(V, expr) */					\
-	template <template <class _V> class _OP, class _L, class _R>	\
-	inline								\
-	expr<ops::sub<vx>, vx, expr<_OP<vx>, _L, _R> >			\
-	operator-(const vx& a, const expr<_OP<vx>, _L, _R>& b) {	\
-		return expr<ops::sub<vx>,				\
-			    vx, expr<_OP<vx>, _L, _R> >(a, b);		\
-	}								\
-	/* operator -(expr, V) */					\
-	template <template <class _V> class _OP, class _L, class _R>	\
-	inline								\
-	expr<ops::sub<vx>, expr<_OP<vx>, _L, _R>, vx>			\
-	operator-(const expr<_OP<vx>, _L, _R>& a, const vx& b) {	\
-		return expr<ops::sub<vx>,				\
-			    expr<_OP<vx>, _L, _R>, vx>(a, b);		\
-	}								\
-	/* operator -(expr, expr)  */					\
-	template <template <class _V> class _OP1, class _L1, class _R1,	\
-		  template <class _V> class _OP2, class _L2, class _R2>	\
-	inline								\
-	expr<ops::sub<vx>,						\
-	     expr<_OP1<vx>, _L1, _R1>, expr<_OP2<vx>, _L2, _R2> >	\
-	operator-(const expr<_OP1<vx>, _L1, _R1>& a,			\
-		  const expr<_OP2<vx>, _L2, _R2>& b) {			\
-		return expr<ops::sub<vx>,				\
-			    expr<_OP1<vx>, _L1, _R1>,			\
-			    expr<_OP2<vx>, _L2, _R2> > (a, b);		\
-	}								\
-	/* operator -= V */						\
-	inline								\
-	vx& operator-=(vx& a, const vx& r) {				\
-		a = a - r;						\
-		return a;						\
-	}								\
-	/* operator += expr */						\
-	template <template <class _V> class _OP, class _L, class _R>	\
-	inline								\
-	vx& operator-=(vx& a, const expr<_OP<vx>, _L, _R>& r) {		\
-		a = a - r;						\
-		return a;						\
-	}								\
-									\
-	/* operator *(V, V) */						\
-	inline								\
-	expr<ops::mul<vx>, vx, vx>					\
-	operator*(const vx& a, const vx& b) {				\
-		return expr<ops::mul<vx>, vx, vx>(a,b);			\
-	}								\
-	/* operator *(V, expr) */					\
-	template <template <class _V> class _OP, class _L, class _R>	\
-	inline								\
-	expr<ops::mul<vx>, vx, expr<_OP<vx>, _L, _R> >			\
-	operator*(const vx& a, const expr<_OP<vx>, _L, _R>& b) {	\
-		return expr<ops::mul<vx>,				\
-			    vx, expr<_OP<vx>, _L, _R> >(a, b);		\
-	}								\
-	/* operator *(expr, V) */					\
-	template <template <class _V> class _OP, class _L, class _R>	\
-	inline								\
-	expr<ops::mul<vx>, expr<_OP<vx>, _L, _R>, vx>			\
-	operator*(const expr<_OP<vx>, _L, _R>& a, const vx& b) {	\
-		return expr<ops::mul<vx>,				\
-			    expr<_OP<vx>, _L, _R>, vx>(a, b);		\
-	}								\
-	/* operator *(expr, expr) */					\
-	template <template <class _V> class _OP1, class _L1, class _R1,	\
-		  template <class _V> class _OP2, class _L2, class _R2>	\
-	inline								\
-	expr<ops::mul<vx>,						\
-	     expr<_OP1<vx>, _L1, _R1>, expr<_OP2<vx>, _L2, _R2> >	\
-	operator*(const expr<_OP1<vx>, _L1, _R1>& a,			\
-		  const expr<_OP2<vx>, _L2, _R2>& b) {			\
-		return expr<ops::mul<vx>,				\
-			    expr<_OP1<vx>, _L1, _R1>,			\
-			    expr<_OP2<vx>, _L2, _R2> > (a, b);		\
-	}								\
-	/* operator *= V */						\
-	inline								\
-	vx& operator*=(vx& a, const vx& r) {				\
-		a = a * r;						\
-		return a;						\
-	}								\
-	/* operator *= expr */						\
-	template <template <class _V> class _OP, class _L, class _R>	\
-	inline								\
-	vx& operator*=(vx& a, const expr<_OP<vx>, _L, _R>& r) {		\
-		a = a * r;						\
-		return a;						\
-	}								\
-									\
-	/* operator /(V, V) */						\
-	inline								\
-	expr<ops::div<vx>, vx, vx>					\
-	operator/(const vx& a, const vx& b) {				\
-		return expr<ops::div<vx>, vx, vx>(a,b);			\
-	}								\
-	/* operator /(V, expr)  */					\
-	template <template <class _V> class _OP, class _L, class _R>	\
-	inline								\
-	expr<ops::div<vx>, vx, expr<_OP<vx>, _L, _R> >			\
-	operator/(const vx& a, const expr<_OP<vx>, _L, _R>& b) {	\
-		return expr<ops::div<vx>,				\
-			    vx, expr<_OP<vx>, _L, _R> >(a, b);		\
-	}								\
-	/* operator /(expr, V) */					\
-	template <template <class _V> class _OP, class _L, class _R>	\
-	inline								\
-	expr<ops::div<vx>, expr<_OP<vx>, _L, _R>, vx>			\
-	operator/(const expr<_OP<vx>, _L, _R>& a, const vx& b) {	\
-		return expr<ops::div<vx>,				\
-			    expr<_OP<vx>, _L, _R>, vx>(a, b);		\
-	}								\
-	/* operator /(expr, expr) */					\
-	template <template <class _V> class _OP1, class _L1, class _R1,	\
-		  template <class _V> class _OP2, class _L2, class _R2>	\
-	inline								\
-	expr<ops::div<vx>,						\
-	     expr<_OP1<vx>, _L1, _R1>, expr<_OP2<vx>, _L2, _R2> >	\
-	operator/(const expr<_OP1<vx>, _L1, _R1>& a,			\
-		  const expr<_OP2<vx>, _L2, _R2>& b) {			\
-		return expr<ops::div<vx>,				\
-			    expr<_OP1<vx>, _L1, _R1>,			\
-			    expr<_OP2<vx>, _L2, _R2> > (a, b);		\
-	}								\
-	/* operator /= V */						\
-	inline								\
-	vx& operator/=(vx& a, const vx& r) {				\
-		a = a / r;						\
-		return a;						\
-	}								\
-	/* operator += expr */						\
-	template <template <class _V> class _OP, class _L, class _R>	\
-	inline								\
-	vx& operator/=(vx& a, const expr<_OP<vx>, _L, _R>& r) {		\
-		a = a / r;						\
-		return a;						\
-	}								\
+        /* operator eq_op V */                                          \
+        inline                                                          \
+        vx& operator eq_op(vx& a, const vx& r) {			\
+                a = a op r;                                             \
+                return a;                                               \
+        }                                                               \
+        /* operator eq_op expr */                                       \
+        template <template <class _V> class _OP, class _L, class _R>    \
+        inline                                                          \
+        vx& operator eq_op(vx& a, const expr<_OP<vx>, _L, _R>& r) {	\
+                a = a op r;                                             \
+                return a;                                               \
+        }
 
+#define DEFINE_X86VEC_FP_OPERATORS(vx)			\
+	DEFINE_X86VEC_FP_OPERATOR(vx, +, +=, add)	\
+	DEFINE_X86VEC_FP_OPERATOR(vx, -, -=, sub)	\
+	DEFINE_X86VEC_FP_OPERATOR(vx, *, *=, mul)	\
+	DEFINE_X86VEC_FP_OPERATOR(vx, /, /=, div) 
 }
 
 // Local variables:
