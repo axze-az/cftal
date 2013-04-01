6826a9a99a68a5c869af27caed57b5c1883ce4d8
axel <axel@18305beb-b1e2-0310-bcd8-b14b5419b1d7>
2013-04-01 05:13:53 +0000

    added d_real.h
    git-svn-id: file:///usr/local/svn/cpplib/trunk/cftal@573 18305beb-b1e2-0310-bcd8-b14b5419b1d7

---
+++ d_real.h
@@ -0,0 +1,14 @@
+#if !defined (__CFTAL_D_REAL_H__)
+#define __CFTAL_D_REAL_H__ 1
+
+#include <cftal/config.h>
+#include <type_traits>
+
+namespace cftal {
+
+}
+
+// Local variables:
+// mode: c++
+// end:
+#endif
