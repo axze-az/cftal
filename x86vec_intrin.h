7c426837832de967b3e6e29341fff236b4b50de0
axel <axel@18305beb-b1e2-0310-bcd8-b14b5419b1d7>
2014-06-21 13:31:01 +0000

    definitions for kdevelop
    git-svn-id: file:///usr/local/svn/cpplib/trunk/cftal@819 18305beb-b1e2-0310-bcd8-b14b5419b1d7

--- x86vec_intrin.h
+++ x86vec_intrin.h
@@ -1,6 +1,14 @@
 #if !defined (__X86VEC_INTRIN_H__)
 #define __X86VEC_INTRIN_H__ 1
 
+#if defined (IN_KDEVELOP_PARSER)
+#define __AVX__ 1
+#define __SSE4_1__ 1
+#define __SSE4_2__ 1
+#define __SSE4A__ 1
+#define __SSSE3__ 1
+#endif
+
 #include <x86intrin.h>
 
 #if defined (__SSE4_1__) && !defined (__POPCNT__)
