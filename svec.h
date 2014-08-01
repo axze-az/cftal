1919c0aa39d912c81bfa1924f96e76f4391c5083
axel <axel@18305beb-b1e2-0310-bcd8-b14b5419b1d7>
2014-08-01 14:30:52 +0000

    weekly alap updates
    git-svn-id: file:///usr/local/svn/cpplib/trunk/cftal@846 18305beb-b1e2-0310-bcd8-b14b5419b1d7

--- svec.h
+++ svec.h
@@ -8,90 +8,173 @@ namespace cftal {
         
         namespace simd {
 
-                namespace impl {
-        
-                        // abstraction of an emulated hardware
-                        // register _T[_N] splitable into to two
-                        // halves
-                        template <typename _T, std::size_t _N>
-                        struct hw_reg {
-                                static_assert(0==(_N & _N-1), 
-                                              "_N is not a power of 2");
-                                using half_type = hw_reg<_T, _N/2>;
-                                half_type _l;
-                                half_type _h;
-                                hw_reg(half_type l, half_type h) 
-                                        : _l(l), _h(h) {} 
-                                hw_reg(const _T& t) 
-                                        : _l(t), _h(t) {}
-                        };
-
-                        // return the lower half of a hw_reg<_T, _N>
-                        template <typename _T, std::size_t _N>
-                        hw_reg<_T, _N/2> lhalf(const hw_reg<_T, _N>& v) {
-                                return v._l;
-                        }
-
-                        // return the higher half of a hw_reg<_T, _N>
-                        template <typename _T, std::size_t _N>
-                        hw_reg<_T, _N/2> hhalf(const hw_reg<_T, _N>& v) {
-                                return v._h;
-                        }
-                        
-                        // specialization for a scalar register
-                        template <typename _T>
-                        struct hw_reg<_T, 1> {
-                                _T _v;
-                                hw_reg(const _T& t) : _v(t) {}
-                        };
-
-                        template <typename _T, std::size_t _N>
-                        class op_add {
-                                using full_type = hw_reg<_T, _N>;
-
-                                static 
-                                full_type 
-                                v(const full_type& a, const full_type& b) {
-                                        return full_type( 
-                                                op_add<_T, _N/2>::v(lhalf(a), 
-                                                                    lhalf(b)),
-                                                op_add<_T, _N/2>::v(hhalf(a), 
-                                                                    hhalf(b)));
-                                }
-                        };
-
-                        template <typename _T>
-                        class op_add<_T, 1> {
-                                using full_type = hw_reg<_T, 1>;
-                                
-                                static
-                                full_type
-                                v(const full_type& a, const full_type& b) {
-                                        return full_type(
-                                                lhalf(a) + lhalf(b),
-                                                hhalf(a) + hhalf(b));
-                                }
-                        };
-
-                }
-
-
-                
                 template <typename _T, std::size_t _N>
                 class vec {
+                public:
+                        using half_type = vec<_T, _N/2>;
+                        using mask_type = vec<bool, _N>;
                         vec() = default;
                         vec(const vec& r) = default;
                         vec(vec&& r) = default;
                         vec& operator=(const vec& r) = default;
                         vec& operator=(vec&& r) = default;
+                        vec(const _T& v);
+                        vec(const _T& v, bool splat);
+                        vec(const half_type& lh, bool splat=true);
+                        vec(const half_type& lh, const half_type& hh);
+                        const half_type& lh() const;
+                        const half_type& hh() const;
                 private:
                         static_assert(0==(_N & _N-1), "_N is not a power of 2");
-                        impl::hw_reg<_T, _N> _v;
+                        vec<_T, _N/2> _l;
+                        vec<_T, _N/2> _h;
                 };
+
+                template <typename _T, std::size_t _N>
+                const typename vec<_T, _N>::half_type&
+                lo_half(const vec<_T, _N>& v);
+
+                template <typename _T, std::size_t _N>
+                const typename vec<_T, _N>::half_type&
+                hi_half(const vec<_T, _N>& v);
+
+                template <typename _T, std::size_t _N>
+                vec<_T, _N> select(const typename vec<_T, _N>::mask_type& m,
+                                   const vec<_T, _N>& on_true,
+                                   const vec<_T, _N>& on_false);
+                
+                /* permute low and high halves of s */
+                template <int _LH, int _HH,
+                          typename _T, std::size_t _N>
+                vec<_T, _N> permute(const vec<_T, _N>& s);
+
+                /* permute low and high halves of s */
+                template <int _LH, int _HH,
+                          typename _T, std::size_t _N>
+                vec<_T, _N> permute(const vec<_T, _N>& s0,
+                                    const vec<_T, _N>& s1);
+                
+
+
+
+                template <typename _T>
+                class vec<_T, 1> {
+                public:
+                        using mask_type = vec<bool, 1>;
+                        vec() = default;
+                        vec(const vec& r) = default;
+                        vec(vec&& r) = default;
+                        vec& operator=(const vec& r) = default;
+                        vec& operator=(vec&& r) = default;
+                        vec(const _T& v);
+                        vec(const _T& v, bool splat);
+                        _T operator()() const;
+                private:
+                        _T _v;
+                };
+
+                template <typename _T>
+                vec<_T, 1> select(const typename vec<_T, 1>::mask_type& m,
+                                  const vec<_T, 1>& on_true,
+                                  const vec<_T, 1>& on_false);
+                
+                namespace ops {
+                        
+                        template <typename _T, std::size_t _N>
+                        class op_add {
+                                using full_type = vec<_T, _N>;
+                                static 
+                                full_type 
+                                v(const full_type& a, const full_type& b) {
+                                        return full_type( 
+                                                op_add<_T, _N/2>::v(lo_half(a), 
+                                                                    lo_half(b)),
+                                                op_add<_T, _N/2>::v(hi_half(a), 
+                                                                    hi_half(b)));
+                                }
+                        };
+
+                        template <typename _T>
+                        class op_add<_T, 1> {
+                                using full_type = vec<_T, 1>;
+                                static
+                                full_type
+                                v(const full_type& a, const full_type& b) {
+                                        return full_type(a() + b());
+                                }
+                        };
+
+                }
                 
         }
-                
 }
+
+template <class _T, std::size_t _N>
+inline
+cftal::simd::vec<_T, _N>::vec(const _T& v)
+        : _l(v), _h(v)
+{
+}
+
+template <class _T, std::size_t _N>
+inline
+cftal::simd::vec<_T, _N>::vec(const _T& v, bool splat)
+        : _l(v, splat), _h(splat ? v : _T(0))
+{
+}
+
+template <class _T, std::size_t _N>
+inline
+cftal::simd::vec<_T, _N>::vec(const half_type& l,
+                              const half_type& h)
+        : _l(l), _h(h)
+{
+}
+
+template <class _T, std::size_t _N>
+inline
+const typename cftal::simd::vec<_T, _N>::half_type&
+cftal::simd::vec<_T, _N>::lh() const
+{
+        return _l;
+}
+
+template <class _T, std::size_t _N>
+inline
+const typename cftal::simd::vec<_T, _N>::half_type&
+cftal::simd::vec<_T, _N>::hh() const
+{
+        return _h;
+}
+
+template <class _T, std::size_t _N>
+inline
+const typename cftal::simd::vec<_T, _N>::half_type&
+cftal::simd::lo_half(const vec<_T, _N>& v) 
+{
+        return v.lh();
+}
+
+template <class _T, std::size_t _N>
+inline
+const typename cftal::simd::vec<_T, _N>::half_type&
+cftal::simd::hi_half(const vec<_T, _N>& v)
+{
+        return v.hh();
+}
+
+
+template <class _T, std::size_t _N>
+inline
+cftal::simd::vec<_T, _N>
+cftal::simd::select(const typename vec<_T, _N>::mask_type& m,
+                    const vec<_T, _N>& on_true,
+                    const vec<_T, _N>& on_false)
+{
+        return vec<_T, _N>();
+}
+
+
 // Local variables:
 // mode: c++
 // end:
