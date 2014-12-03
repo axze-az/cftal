8e941c77384fa6fde4ddf544635ba71e819e6e77
axel <axel@18305beb-b1e2-0310-bcd8-b14b5419b1d7>
2014-12-03 11:11:00 +0000

    more reorganizations
    git-svn-id: file:///usr/local/svn/cpplib/trunk/cftal@906 18305beb-b1e2-0310-bcd8-b14b5419b1d7

--- old/emuvec_inl.h
+++
@@ -1,5 +0,0 @@
-#if !defined (__EMUVEC_INL_H__)
-#define __EMUVEC_INL_H__ 1
-
-#endif
-
--- old/x86vec_v4f64.cc
+++
@@ -1,5 +0,0 @@
-#include "x86vec_traits.h"
-#include "math_func.h"
-#include "x86vec_math_funcs.h"
-
-DEF_MATH_FUNCS(x86vec, v4f64, v4s32, double, 6)
