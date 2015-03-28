0ea9b8c648f48325f8cde55e960a0fbc0e15d714
axel <axel@18305beb-b1e2-0310-bcd8-b14b5419b1d7>
2015-03-28 14:24:47 +0000

    more changes
    git-svn-id: file:///usr/local/svn/cpplib/trunk/cftal@984 18305beb-b1e2-0310-bcd8-b14b5419b1d7

--- x86_v8f32.cc
+++ x86_v8f32.cc
@@ -1,10 +1,10 @@
 #if defined (__AVX__)
-#include "x86_traits.h"
+#include "vec_traits.h"
 #include "math_func.h"
 #include "d_real.h"
 #include <cmath>
 #include <limits>
-#include "x86_math_funcs.h"
+#include "vec_math_funcs.h"
 
 DEF_MATH_FUNCS(cftal, v8f32, v8s32, float, 4)
 
