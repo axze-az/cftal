5617409d9cb5c4a551f878f1c8507d5266bd8155
axel <axel@18305beb-b1e2-0310-bcd8-b14b5419b1d7>
2015-04-05 05:47:54 +0000

    reorder the includes in vec.h
    git-svn-id: file:///usr/local/svn/cpplib/trunk/cftal@989 18305beb-b1e2-0310-bcd8-b14b5419b1d7

--- vec.h
+++ vec.h
@@ -14,21 +14,15 @@
 #include <cmath>
 
 #include <cftal/vec_t_1.h>
-#include <cftal/vec_double_n.h>
-#include <cftal/vec_float_n.h>
-
-// include vector specializations
-#if defined (__SSE__)
-#include <cftal/x86_v4f32.h>
-#endif
 #if defined (__SSE2__)
+// include 128 bit integer vector specializations 
 #include <cftal/x86_v4s32.h>
 #include <cftal/x86_v4u32.h>
 #include <cftal/x86_v2s64.h>
 #include <cftal/x86_v2u64.h>
 #include <cftal/x86_v8s16.h>
 #include <cftal/x86_v8u16.h>
-#include <cftal/x86_v2f64.h>
+// include 128 bit integer vector specializations implementations
 #include <cftal/x86_v4s32_inl.h>
 #include <cftal/x86_v4u32_inl.h>
 #include <cftal/x86_v2s64_inl.h>
@@ -37,28 +31,30 @@
 #include <cftal/x86_v8u16_inl.h>
 #endif
 #if defined (__AVX2__)
+// include 256 bit integer vector specializations
 #include <cftal/x86_v8s32.h>
 #include <cftal/x86_v8u32.h>
 #include <cftal/x86_v4s64.h>
 #include <cftal/x86_v4u64.h>
+// include 256 bit integer vector specializations implementations
 #include <cftal/x86_v8s32_inl.h>
 #include <cftal/x86_v8u32_inl.h>
 #include <cftal/x86_v4s64_inl.h>
 #include <cftal/x86_v4u64_inl.h>
 #endif
+#include <cftal/vec_double_n.h>
+#if defined (__SSE2__)
+#include <cftal/x86_v2f64.h>
+#endif
 #if defined (__AVX__)
-#include <cftal/x86_v8f32.h>
 #include <cftal/x86_v4f64.h>
 #endif
-#if defined (__AVX512F__)
-#include <cftal/x86_v8f64.h>
-#if 0
-#include <cftal/x86_v16f32.h>
-#include <cftal/x86_v16s32.h>
-#include <cftal/x86_v16u32.h>
-#include <cftal/x86_v8s64.h>
-#include <cftal/x86_v8u64.h>
+#include <cftal/vec_float_n.h>
+#if defined (__SSE__)
+#include <cftal/x86_v4f32.h>
 #endif
+#if defined (__AVX__)
+#include <cftal/x86_v8f32.h>
 #endif
 
 #include <cftal/x86_cvt.h>
