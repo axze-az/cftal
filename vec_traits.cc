c47006a70dce91292e316326b583ffaa7113f08c
axel <axel@18305beb-b1e2-0310-bcd8-b14b5419b1d7>
2015-04-06 05:07:35 +0000

    lost file
    git-svn-id: file:///usr/local/svn/cpplib/trunk/cftal@994 18305beb-b1e2-0310-bcd8-b14b5419b1d7

---
+++ vec_traits.cc
@@ -0,0 +1,18 @@
+#include "vec_traits.h"
+
+
+const cftal::v4s32
+cftal::math::func_traits<cftal::v2f64, cftal::v2s32>::_vmi_to_vmf_msk[4]= {
+    {  0,  0,  0,  0},
+    { -1, -1,  0,  0},
+    {  0,  0, -1, -1},
+    { -1, -1, -1, -1}
+};
+
+const cftal::vec<bool, 2>
+cftal::math::func_traits<cftal::v2f64, cftal::v2s32>::_vmf_to_vmi_msk[4]= {
+    {false, false},
+    {true, false},
+    {false, true},
+    {true, true}
+};
