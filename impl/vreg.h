d82253f1f8365e574cedc8c808b5052e2e3f2264
axel <axel@18305beb-b1e2-0310-bcd8-b14b5419b1d7>
2015-11-15 09:41:18 +0000

    more updates
    git-svn-id: file:///usr/local/svn/cpplib/trunk/cftal@1111 18305beb-b1e2-0310-bcd8-b14b5419b1d7

--- impl/vreg.h
+++ impl/vreg.h
@@ -1,14 +1,11 @@
-#if !defined (__CFTAL_X86_VREG_H__)
-#define __CFTAL_X86_VREG_H__ 1
+#if !defined (__CFTAL_IMPL_VREG_H__)
+#define __CFTAL_IMPL_VREG_H__ 1
 
 #include <cftal/config.h>
 
 namespace cftal {
 
-    namespace x86 {
-
-        template <typename _T>
-        struct arg { using type = _T; };
+    namespace impl {
 
         // wrapper around a vector register type
         template <class _R>
@@ -32,20 +29,20 @@ namespace cftal {
 template <class _X>
 inline
 constexpr
-cftal::x86::vreg<_X>::vreg(_X x) : _v(x)
+cftal::impl::vreg<_X>::vreg(_X x) : _v(x)
 {
 }
 
 template <class _X>
 inline
-_X cftal::x86::vreg<_X>::operator()() const
+_X cftal::impl::vreg<_X>::operator()() const
 {
     return _v;
 }
 
 template <class _X>
 inline
-_X& cftal::x86::vreg<_X>::operator()()
+_X& cftal::impl::vreg<_X>::operator()()
 {
     return _v;
 }
--- x86/vreg.h
+++ x86/vreg.h
@@ -2,6 +2,7 @@
 #define __CFTAL_X86_VREG_H__ 1
 
 #include <cftal/config.h>
+#include <cftal/impl/vreg.h>
 
 namespace cftal {
 
@@ -9,45 +10,8 @@ namespace cftal {
 
         template <typename _T>
         struct arg { using type = _T; };
-
-        // wrapper around a vector register type
-        template <class _R>
-        class vreg {
-            _R _v;
-        public:
-            using vector_type = _R;
-            using element_type = void;
-            vreg() = default;
-            vreg(const vreg& r) = default;
-            vreg(vreg&& r) = default;
-            vreg& operator=(const vreg& r) = default;
-            vreg& operator=(vreg&& r) = default;
-            constexpr vreg(_R x);
-            _R operator()() const;
-            _R& operator()();
-        };
+		using ::cftal::impl::vreg;
     }
-}
-
-template <class _X>
-inline
-constexpr
-cftal::x86::vreg<_X>::vreg(_X x) : _v(x)
-{
-}
-
-template <class _X>
-inline
-_X cftal::x86::vreg<_X>::operator()() const
-{
-    return _v;
-}
-
-template <class _X>
-inline
-_X& cftal::x86::vreg<_X>::operator()()
-{
-    return _v;
 }
 
 // Local variables:
