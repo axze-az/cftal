c39ab7a8069b099b1b55ad767b54138a26e010fe
axel <axel@18305beb-b1e2-0310-bcd8-b14b5419b1d7>
2015-12-09 06:33:41 +0000

    aarch64: 128 bit permutations
    git-svn-id: file:///usr/local/svn/cpplib/trunk/cftal@1221 18305beb-b1e2-0310-bcd8-b14b5419b1d7

--- include/cftal/arm/perm.h
+++ include/cftal/arm/perm.h
@@ -35,6 +35,36 @@ namespace cftal {
                 uint32x2_t v(uint32x2_t a, uint32x2_t b);
             };
 
+            template <int _P0, int _P1, int _P2, int _P3,
+                      int _P4, int _P5, int _P6, int _P7,
+                      int _P8, int _P9, int _PA, int _PB,
+                      int _PC, int _PD, int _PE, int _PF>
+            struct perm1_v16u8 {
+                static
+                uint8x16_t v(uint8x16_t a);
+            };
+
+            template <int _P0, int _P1, int _P2, int _P3,
+                      int _P4, int _P5, int _P6, int _P7,
+                      int _P8, int _P9, int _PA, int _PB,
+                      int _PC, int _PD, int _PE, int _PF>
+            struct perm2_v16u8 {
+                static
+                uint8x16_t v(uint8x16_t a, uint8x16_t b);
+            };
+
+            template <int _P0, int _P1, int _P2, int _P3>
+            struct perm1_v4u32 {
+                static
+                uint32x4_t v(uint32x4_t a);
+            };
+
+            template <int _P0, int _P1, int _P2, int _P3>
+            struct perm2_v4u32 {
+                static
+                uint32x4_t v(uint32x4_t a, uint32x4_t b);
+            };
+
         }
 
         template <int _P0, int _P1, int _P2, int _P3,
@@ -145,6 +175,109 @@ cftal::arm::impl::perm2_v2u32<_P0, _P1>::v(uint32x2_t a, uint32x2_t b)
     uint8x8_t ri=perm2_v8u8<u0, u1, u2, u3, u4, u5, u6, u7>::v(ai, bi);
     return vreinterpret_u32_u8(ri);
 }
+
+template <int _P0, int _P1, int _P2, int _P3,
+          int _P4, int _P5, int _P6, int _P7,
+          int _P8, int _P9, int _PA, int _PB,
+          int _PC, int _PD, int _PE, int _PF>
+inline
+uint8x16_t
+cftal::arm::impl::
+perm1_v16u8<_P0, _P1, _P2, _P3, _P4, _P5, _P6, _P7,
+            _P8, _P9, _PA, _PB, _PC, _PD, _PE, _PF>::v(uint8x16_t a)
+{
+    uint8x16_t dst{uint8_t(_P0), uint8_t(_P1), uint8_t(_P2), uint8_t(_P3),
+                   uint8_t(_P4), uint8_t(_P5), uint8_t(_P6), uint8_t(_P7),
+                   uint8_t(_P8), uint8_t(_P9), uint8_t(_PA), uint8_t(_PB),
+                   uint8_t(_PC), uint8_t(_PD), uint8_t(_PE), uint8_t(_PF)};
+    uint8x16_t ri=vqtbl1q_u8(a, dst);
+    return ri;
+}
+
+template <int _P0, int _P1, int _P2, int _P3,
+          int _P4, int _P5, int _P6, int _P7,
+          int _P8, int _P9, int _PA, int _PB,
+          int _PC, int _PD, int _PE, int _PF>
+inline
+uint8x16_t
+cftal::arm::impl::
+perm2_v16u8<_P0, _P1, _P2, _P3, _P4, _P5, _P6, _P7,
+            _P8, _P9, _PA, _PB, _PC, _PD, _PE, _PF>::v(uint8x16_t a,
+                                                       uint8x16_t b)
+{
+    uint8x16_t dst{uint8_t(_P0), uint8_t(_P1), uint8_t(_P2), uint8_t(_P3),
+                   uint8_t(_P4), uint8_t(_P5), uint8_t(_P6), uint8_t(_P7),
+                   uint8_t(_P8), uint8_t(_P9), uint8_t(_PA), uint8_t(_PB),
+                   uint8_t(_PC), uint8_t(_PD), uint8_t(_PE), uint8_t(_PF)};
+    uint8x16x2_t ab{a, b};
+    uint8x8_t ri=vqtbl2q_u8(ab, dst);
+    return ri;
+}
+
+template <int _P0, int _P1, int _P2, int _P3>
+inline
+uint32x4_t
+cftal::arm::impl::perm1_v4u32<_P0, _P1, _P2, _P3>::v(uint32x4_t a)
+{
+    const bool p0n = _P0 < 0;
+    const bool p1n = _P1 < 0;
+    const bool p2n = _P2 < 0;
+    const bool p3n = _P3 < 0;
+    const int u0= p0n ? -1 : (4*_P0) + 0;
+    const int u1= p0n ? -1 : (4*_P0) + 1;
+    const int u2= p0n ? -1 : (4*_P0) + 2;
+    const int u3= p0n ? -1 : (4*_P0) + 3;
+    const int u4= p1n ? -1 : (4*_P1) + 0;
+    const int u5= p1n ? -1 : (4*_P1) + 1;
+    const int u6= p1n ? -1 : (4*_P1) + 2;
+    const int u7= p1n ? -1 : (4*_P1) + 3;
+    const int u8= p2n ? -1 : (4*_P2) + 0;
+    const int u9= p2n ? -1 : (4*_P2) + 1;
+    const int ua= p2n ? -1 : (4*_P2) + 2;
+    const int ub= p2n ? -1 : (4*_P2) + 3;
+    const int uc= p3n ? -1 : (4*_P3) + 0;
+    const int ud= p3n ? -1 : (4*_P3) + 1;
+    const int ue= p3n ? -1 : (4*_P3) + 2;
+    const int uf= p3n ? -1 : (4*_P3) + 3;
+    uint8x16_t ai = vreinterpretq_u8_u32(a);
+    uint8x16_t ri=perm1_v16u8<u0, u1, u2, u3, u4, u5, u6, u7,
+                              u8, u9, ua, ub, uc, ud, ue, uf>::v(ai);
+    return vreinterpretq_u32_u8(ri);
+}
+
+template <int _P0, int _P1, int _P2, int _P3>
+inline
+uint32x4_t
+cftal::arm::impl::
+perm2_v4u32<_P0, _P1, _P2, _P3>::v(uint32x4_t a, uint32x4_t b)
+{
+    const bool p0n = _P0 < 0;
+    const bool p1n = _P1 < 0;
+    const bool p2n = _P2 < 0;
+    const bool p3n = _P3 < 0;
+    const int u0= p0n ? -1 : (4*_P0) + 0;
+    const int u1= p0n ? -1 : (4*_P0) + 1;
+    const int u2= p0n ? -1 : (4*_P0) + 2;
+    const int u3= p0n ? -1 : (4*_P0) + 3;
+    const int u4= p1n ? -1 : (4*_P1) + 0;
+    const int u5= p1n ? -1 : (4*_P1) + 1;
+    const int u6= p1n ? -1 : (4*_P1) + 2;
+    const int u7= p1n ? -1 : (4*_P1) + 3;
+    const int u8= p2n ? -1 : (4*_P2) + 0;
+    const int u9= p2n ? -1 : (4*_P2) + 1;
+    const int ua= p2n ? -1 : (4*_P2) + 2;
+    const int ub= p2n ? -1 : (4*_P2) + 3;
+    const int uc= p3n ? -1 : (4*_P3) + 0;
+    const int ud= p3n ? -1 : (4*_P3) + 1;
+    const int ue= p3n ? -1 : (4*_P3) + 2;
+    const int uf= p3n ? -1 : (4*_P3) + 3;
+    uint8x16_t ai = vreinterpretq_u8_u32(a);
+    uint8x16_t bi = vreinterpretq_u8_u32(b);
+    uint8x16_t ri=perm2_v16u8<u0, u1, u2, u3, u4, u5, u6, u7,
+                              u8, u9, ua, ub, uc, ud, ue, uf>::v(ai, bi);
+    return vreinterpretq_u32_u8(ri);
+}
+
 
 template <int _P0, int _P1, int _P2, int _P3,
           int _P4, int _P5, int _P6, int _P7>
