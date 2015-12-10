41d53c9a9bf3763d357b44dd7a3e32e91caf9ab9
axel <axel@18305beb-b1e2-0310-bcd8-b14b5419b1d7>
2015-12-10 08:38:02 +0000

    vXs16/u16: use 32 elements as maximum vector length
    git-svn-id: file:///usr/local/svn/cpplib/trunk/cftal@1240 18305beb-b1e2-0310-bcd8-b14b5419b1d7

--- test/test-ops-vXs16.cc
+++ test/test-ops-vXs16.cc
@@ -4,7 +4,7 @@ int main()
 {
     bool rc=true;
     std::cout << "testing vXs16" << std::endl;
-    rc=cftal::test::of_ops_up_to<int16_t, 16>::v();
+    rc=cftal::test::of_ops_up_to<int16_t, 32>::v();
     if (rc==false)
         std::cerr << "int16_t test failed" << std::endl;
     return rc==true ? 0 : 1;
