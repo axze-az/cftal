d88360c5516d72a7deff51a071ccbfbe9eddcae7
axel <axel@18305beb-b1e2-0310-bcd8-b14b5419b1d7>
2015-12-10 06:51:13 +0000

    avoid integer division by 0
    git-svn-id: file:///usr/local/svn/cpplib/trunk/cftal@1234 18305beb-b1e2-0310-bcd8-b14b5419b1d7

--- include/cftal/vec_t_1.h
+++ include/cftal/vec_t_1.h
@@ -263,13 +263,33 @@ namespace cftal {
             }
         };
 
+        namespace impl {
+
+            template <class _T>
+            std::enable_if_t<std::is_integral<_T>::value, _T>
+            divide(const _T& a, const _T& b) {
+                if (b == _T(0))
+                    return ~_T(0);
+                return a/b;
+            }
+
+            template <class _T>
+            std::enable_if_t<std::is_floating_point<_T>::value, _T>
+            divide(const _T& a, const _T& b) {
+                return a/b;
+            }
+
+
+        }
+
         template <typename _T>
         struct div<_T, 1> {
             using full_type = vec<_T, 1>;
             static
             full_type
             v(const full_type& a, const full_type& b) {
-                return full_type(a() / b());
+                _T q=impl::divide(a(), b());
+                return full_type(q);
             }
         };
 
