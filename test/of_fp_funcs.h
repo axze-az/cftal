3ead42b9c685dbe92f782f2b2d8453cffcf1cb61
axel <axel@18305beb-b1e2-0310-bcd8-b14b5419b1d7>
2015-12-31 06:33:25 +0000

    test: allow ranges in cftal::test::of_fp_funcs::v
    git-svn-id: file:///usr/local/svn/cpplib/trunk/cftal@1302 18305beb-b1e2-0310-bcd8-b14b5419b1d7

--- test/of_fp_funcs.h
+++ test/of_fp_funcs.h
@@ -8,23 +8,42 @@
 #include <random>
 #include <iostream>
 #include <cmath>
+#include <utility>
 
 namespace cftal {
     namespace test {
 
+        template <typename _T>
+        using func_arg_range = std::pair<_T, _T>;
+
+        template <typename _T>
+        struct default_range {
+            static
+            const func_arg_range<_T> value;
+        };
+
+        template <typename _T>
+        const func_arg_range<_T>
+        default_range<_T>::value =
+            std::make_pair(std::numeric_limits<_T>::lowest(),
+                           std::numeric_limits<_T>::max());
+
         template <typename _T, std::size_t _N, typename _F>
         struct of_fp_func {
-            static bool v();
-            static bool v(_T ai);
+            static
+            bool
+            v(func_arg_range<_T> arg_range = default_range<_T>::value);
+            static
+            bool v(_T ai);
         };
 
         template <typename _T, std::size_t _N, typename _F>
         struct of_fp_func_up_to {
             static
             bool
-            v() {
-                bool r=of_fp_func<_T, _N, _F>::v();
-                r &= of_fp_func_up_to<_T, _N/2, _F>::v();
+            v(func_arg_range<_T> arg_range = default_range<_T>::value) {
+                bool r=of_fp_func<_T, _N, _F>::v(arg_range);
+                r &= of_fp_func_up_to<_T, _N/2, _F>::v(arg_range);
                 return r;
             }
         };
@@ -33,8 +52,8 @@ namespace cftal {
         struct of_fp_func_up_to<_T, 1, _F> {
             static
             bool
-            v() {
-                return of_fp_func<_T, 1, _F>::v();
+            v(func_arg_range<_T> arg_range = default_range<_T>::value) {
+                return of_fp_func<_T, 1, _F>::v(arg_range);
             }
         };
 
@@ -125,7 +144,7 @@ cftal::test::of_fp_func<_T, _N, _F>::v(_T a)
 
 template <typename _T, std::size_t _N, typename _F>
 bool
-cftal::test::of_fp_func<_T, _N, _F>::v()
+cftal::test::of_fp_func<_T, _N, _F>::v(func_arg_range<_T> arg_range)
 {
     bool r = true;
     _T a;
@@ -147,16 +166,31 @@ cftal::test::of_fp_func<_T, _N, _F>::v()
 
     std::mt19937 rnd;
     std::uniform_real_distribution<_T>
-        distrib(0, std::numeric_limits<_T>::max());
+        distrib(arg_range.first, arg_range.second);
 
-    const int64_t N0=0x10000ULL;
+    std::cout << "[" << arg_range.first << ", " << arg_range.second << ")\n";
+    const int64_t N0=0x20000ULL;
     const int64_t N=72*N0;
     for (int64_t i=0; i<N; ++i) {
         if ((i & (N0-1)) == (N0-1))
             std::cout << '.' << std::flush;
         _T ah=distrib(rnd);
         r &= v(ah);
-        r &= v(-ah);
+    }
+    _T minus1= std::max(_T(-1), arg_range.first);
+    _T plus1= std::min(_T(1), arg_range.second);
+    if (minus1 < plus1) {
+        std::cout << std::endl;
+        _T nplus1=std::nextafter(plus1, _T(2)*plus1);
+        std::uniform_real_distribution<_T>
+            distrib1(minus1, nplus1);
+        std::cout << "[" << minus1 << ", " << nplus1 << ")\n";
+        for (int64_t i=0; i<N; ++i) {
+            if ((i & (N0-1)) == (N0-1))
+                std::cout << '.' << std::flush;
+            _T ah=distrib1(rnd);
+            r &= v(ah);
+        }
     }
     std::cout << std::endl;
     if (r == true) {
