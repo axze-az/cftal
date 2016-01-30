86c97cc4f111641cb2bba67d3b130ec63bb8922b
axel <axel@18305beb-b1e2-0310-bcd8-b14b5419b1d7>
2016-01-30 10:20:40 +0000

    more cbrt tests
    git-svn-id: file:///usr/local/svn/cpplib/trunk/cftal@1413 18305beb-b1e2-0310-bcd8-b14b5419b1d7

--- hackx86vec.cc
+++ hackx86vec.cc
@@ -2,8 +2,21 @@
 #include <iomanip>
 #include <cstddef>
 #include <iterator>
+#include <cmath>
 
 int main(int argc, char** argv)
 {
+
+    double t= 1.0;
+    int e0;
+
+    double m=std::frexp(t, &e0);
+
+    int e1=std::ilogb(t);
+
+    std::cout << std::scientific << std::setprecision(22);
+    std::cout << m << "* 2^" << e0 << std::endl;
+    std::cout << e1 << std::endl;
+
     return 0;
 }
--- test/test-cbrt.cc
+++ test/test-cbrt.cc
@@ -248,7 +248,7 @@ int main(int argc, char** argv)
                                           std::numeric_limits<double>::max());
     auto us=std::make_shared<ulp_stats>();
     rc &= of_fp_func_up_to<
-        double, 8, check_cbrt<double> >::v(dd, cmp_ulp<double>(1, us));
+        double, 8, check_cbrt<double> >::v(dd, cmp_ulp<double>(0, us));
     std::cout << "ulps: "
               << std::fixed << std::setprecision(4) << *us << std::endl;
 
@@ -257,7 +257,7 @@ int main(int argc, char** argv)
                                           std::numeric_limits<float>::max());
     us=std::make_shared<ulp_stats>();
     rc &= of_fp_func_up_to<
-        float, 8, check_cbrt<float> >::v(df, cmp_ulp<float>(1, us));
+        float, 8, check_cbrt<float> >::v(df, cmp_ulp<float>(0, us));
     std::cout << "ulps: "
               << std::fixed << std::setprecision(4) << *us << std::endl;
 
