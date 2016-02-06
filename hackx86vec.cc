de3e291706006d3dea216619d39542f2222524cd
axel <axel@18305beb-b1e2-0310-bcd8-b14b5419b1d7>
2016-02-06 15:35:10 +0000

    a different hack
    git-svn-id: file:///usr/local/svn/cpplib/trunk/cftal@1434 18305beb-b1e2-0310-bcd8-b14b5419b1d7

--- hackx86vec.cc
+++ hackx86vec.cc
@@ -3,20 +3,25 @@
 #include <cstddef>
 #include <iterator>
 #include <cmath>
+#include <cftal/vec.h>
 
 int main(int argc, char** argv)
 {
+    using namespace cftal;
+    v2f64 a(3.880996418503809764e+02, -4.217746855610766374e+02);
+    v2f64 r=exp(a);
+    v2f64 re0(std::exp(3.880996418503809764e+02), std::exp(-4.217746855610766374e+02));
 
-    double t= 1.0;
-    int e0;
+    std::cout << std::scientific<< std::setprecision(22);
+    std::cout << a << std::endl;
+    std::cout << r << std::endl;
+    std::cout << re0 << std::endl;
 
-    double m=std::frexp(t, &e0);
-
-    int e1=std::ilogb(t);
-
-    std::cout << std::scientific << std::setprecision(22);
-    std::cout << m << "* 2^" << e0 << std::endl;
-    std::cout << e1 << std::endl;
-
+    a = v2f64(1.0, 2.0);
+    r = exp(a);
+    v2f64 re1(std::exp(1.0), std::exp(2.0));
+    std::cout << a << std::endl;
+    std::cout << r << std::endl;
+    std::cout << re1 << std::endl;
     return 0;
 }
