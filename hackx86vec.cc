ebb912ac4917b39ea9f2f40c9b52edcd295dd583
axel <axel@18305beb-b1e2-0310-bcd8-b14b5419b1d7>
2016-02-14 14:11:31 +0000

    missing compile check
    git-svn-id: file:///usr/local/svn/cpplib/trunk/cftal@1456 18305beb-b1e2-0310-bcd8-b14b5419b1d7

--- hackx86vec.cc
+++ hackx86vec.cc
@@ -12,15 +12,6 @@
 int main(int argc, char** argv)
 {
     using namespace cftal;
-    std::cout << std::scientific<< std::setprecision(22);
-    v1f64 u(6.349540949350158986e-276), v(2.565091036874223535e+44);
-    // v1f64 v(-4.412109017717083848e-283), u(-2.292371590037048002e-309);
-    // v1f64 u(2.417695834957350489e-308), v(1.306809459378748165e-13);
-    using traits_t=cftal::math::func_traits<v1f64,v1s32>;
-    using func_t=cftal::math::func<double, traits_t>;
-    d_real<v1f64> q=func_t::scaled_divide(u, v1f64(0), v, v1f64(0));
-
-    std::cout << q.h() << " " << q.l() << std::endl;
 
     v1f64 a(2.417695834957350489e-308), b(1.306809459378748165e-13);
     v1f64 r=atan2(a, b);
