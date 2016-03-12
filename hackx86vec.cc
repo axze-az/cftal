59b39c7446276f9977e44b0e05a65f3f1afb548a
axel <axel@18305beb-b1e2-0310-bcd8-b14b5419b1d7>
2016-03-12 15:35:47 +0000

    more test functions
    git-svn-id: file:///usr/local/svn/cpplib/trunk/cftal@1517 18305beb-b1e2-0310-bcd8-b14b5419b1d7

--- hackx86vec.cc
+++ hackx86vec.cc
@@ -14,20 +14,23 @@ namespace cftal {
 
         template <typename _T, std::size_t _N, typename _X, std::size_t _L>
         struct lookup {
+            // lookup with default value
             static
             vec<_T, _N>
             v(const vec<_T, _N>& def, const vec<int32_t, _N>& idx, const _X* p);
         };
-
+        
         template <typename _T, std::size_t _N, typename _X>
         struct lookup<_T, _N, _X, 0> {
+            // lookup with default value
             static
             vec<_T, _N>
             v(const vec<_T, _N>& def, const vec<int32_t, _N>& idx, const _X* p);
         };
 
     }
-    
+
+    // lookup with default value
     template <typename _T, std::size_t _N, typename _X, std::size_t _L>
     vec<_T, _N>
     lookup(const vec<_T, _N>& def, const vec<int32_t, _N>& idx, const _X (&r)[_L]);
@@ -40,7 +43,9 @@ cftal::impl::lookup<_T, _N, _X, 0>::
 v(const vec<_T, _N>& def, const vec<int32_t, _N>& idx, const _X* p)
 {
     typename vec<int32_t, _N>::mask_type idx_eq_0 = idx == vec<int32_t, _N>(0);
-    typename vec<_T, _N>::mask_type f_idx_eq_0= cvt_mask<_T, int32_t, _N>::v(idx_eq_0);
+    typename vec<_T, _N>::mask_type f_idx_eq_0=
+        cvt_mask<typename vec<_T, _N>::mask_type::value_type,
+                 typename vec<int32_t, _N>::mask_type::value_type, _N>::v(idx_eq_0);
     vec<_T, _N> r_idx_eq_0(p[0]);
     vec<_T, _N> r = select(f_idx_eq_0, r_idx_eq_0, def);
     return r;
@@ -55,7 +60,9 @@ v(const vec<_T, _N>& def, const vec<int32_t, _N>& idx, const _X* p)
     // lookup all lower values to zero:
     vec<_T, _N> rl= lookup<_T, _N, _X, _LM1>::v(def, idx, p);
     typename vec<int32_t, _N>::mask_type idx_eq_l = idx == vec<int32_t, _N>(_L);
-    typename vec<_T, _N>::mask_type f_idx_eq_l= cvt_mask<_T, int32_t, _N>::v(idx_eq_l);
+    typename vec<_T, _N>::mask_type f_idx_eq_l=
+        cvt_mask<typename vec<_T, _N>::mask_type::value_type,
+                 typename vec<int32_t, _N>::mask_type::value_type, _N>::v(idx_eq_l);
     vec<_T, _N> r_idx_eq_l(p[_L]);
     vec<_T, _N> r= select(f_idx_eq_l, r_idx_eq_l, rl);
     return r;
