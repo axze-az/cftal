5ceb528915e83cc65edc621cf5cf8b8bac0a9f12
axel <axel@18305beb-b1e2-0310-bcd8-b14b5419b1d7>
2016-03-28 07:19:25 +0000

    add the preferred range test also to f64 sin and cos
    git-svn-id: file:///usr/local/svn/cpplib/trunk/cftal@1576 18305beb-b1e2-0310-bcd8-b14b5419b1d7

--- test/test-cos-f64.cc
+++ test/test-cos-f64.cc
@@ -37,5 +37,17 @@ int main(int argc, char** argv)
     std::cout << "ulps: "
               << std::fixed << std::setprecision(4) << *us << std::endl;
     std::cout << st << std::endl;
+
+    exec_stats st2(_N);
+    auto dp2=std::make_pair(-0x1p20, 0x1p20);
+    auto us2=std::make_shared<ulp_stats>();
+    rc &= of_fp_func_up_to<
+        double, _N, check_cos<double> >::v(st2, dp2,
+                                           cmp_ulp<double>(ulp, us2),
+                                           0x80000);
+    std::cout << "ulps: "
+              << std::fixed << std::setprecision(4) << *us2 << std::endl;
+    std::cout << st2 << std::endl;
+
     return (rc == true) ? 0 : 1;
 }
