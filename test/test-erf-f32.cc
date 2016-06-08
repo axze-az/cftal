841f33000a77e8320a053e3e5866a29d8c6a89a8
axel <axel@18305beb-b1e2-0310-bcd8-b14b5419b1d7>
2016-06-08 16:53:28 +0000

    halfweekly alap update
    git-svn-id: file:///usr/local/svn/cpplib/trunk/cftal@1708 18305beb-b1e2-0310-bcd8-b14b5419b1d7

--- test/test-erf-f32.cc
+++ test/test-erf-f32.cc
@@ -8,16 +8,17 @@ int main(int argc, char** argv)
     using namespace cftal::test;
     std::cout << std::setprecision(18) << std::scientific;
     std::cerr << std::setprecision(18) << std::scientific;
-    const int ulp=1;
-    const int _N=8;
+    const int ulp=4;
+    const int _N=1;
     bool rc=true;
     bool speed_only=false;
-    std::size_t cnt=0x8000;
+    std::size_t cnt=0x800;
     if ((argc > 1) && (std::string(argv[1]) == "--speed")) {
         speed_only=true;
         cnt *=8;
     }
-    func_domain<float> d=std::make_pair(-25.0f, 25.0f);
+    // func_domain<float> d=std::make_pair(-4.0f, 4.0f);
+    func_domain<float> d=std::make_pair(0, 0.75f);
     auto us=std::make_shared<ulp_stats>();
     exec_stats st(_N);
     rc &= of_fp_func_up_to<
