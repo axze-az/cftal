e9247642b61b72acdcef1f58e23cc8104821506d
axel <axel@18305beb-b1e2-0310-bcd8-b14b5419b1d7>
2016-07-11 16:25:04 +0000

    f64: erf uses now rounded x0 in the intervals f64: erfc from [0, 1.875]
    git-svn-id: file:///usr/local/svn/cpplib/trunk/cftal@1791 18305beb-b1e2-0310-bcd8-b14b5419b1d7

--- test/test-erfc-f64.cc
+++ test/test-erfc-f64.cc
@@ -6,7 +6,7 @@
 int main(int argc, char** argv)
 {
     using namespace cftal::test;
-    const int ulp=32;
+    const int ulp=128;
     const int _N=1;
     bool speed_only=false;
     std::cout << std::setprecision(18) << std::scientific;
@@ -34,7 +34,7 @@ int main(int argc, char** argv)
     rc &= check_func_1<double, 8, check_erf<double> >(v, ulp, 0, false);
 #endif
     // mnewton(erfc(x)-erf(x), x, 0.46);
-    func_domain<double> d=std::make_pair(0.0, 0.75);
+    func_domain<double> d=std::make_pair(1.875, 3.5);
     // func_domain<double> d=std::make_pair(0, 0.75);
     // func_domain<double> d=std::make_pair(-6.0, 27.7);
     auto us=std::make_shared<ulp_stats>();
