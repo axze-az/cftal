d0ce7cb46b5bf153c2ffc3e1188aa35468f962cb
axel <axel@18305beb-b1e2-0310-bcd8-b14b5419b1d7>
2016-09-18 16:25:03 +0000

    exp_mx2-f32 correction
    git-svn-id: file:///usr/local/svn/cpplib/trunk/cftal@1859 18305beb-b1e2-0310-bcd8-b14b5419b1d7

--- test/test-exp_mx2-f32.cc
+++ test/test-exp_mx2-f32.cc
@@ -17,7 +17,7 @@ int main(int argc, char** argv)
         speed_only=true;
         cnt *=8;
     }
-    func_domain<float> d=std::make_pair(10.0, 10.2);
+    func_domain<float> d=std::make_pair(0.0, 10.2);
     exec_stats st(_N);
     auto us=std::make_shared<ulp_stats>();
     rc &= of_fp_func_up_to<
