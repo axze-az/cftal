f89d4b87923199777fe906112757df7f089f84e4
axel <axel@18305beb-b1e2-0310-bcd8-b14b5419b1d7>
2016-11-06 09:09:35 +0000

    more testcode
    git-svn-id: file:///usr/local/svn/cpplib/trunk/cftal@1882 18305beb-b1e2-0310-bcd8-b14b5419b1d7

--- hackx86vec.cc
+++ hackx86vec.cc
@@ -7,9 +7,17 @@
 #include <cftal/d_real.h>
 #include <cftal/vec_traits.h>
 #include <cftal/test/call_mpfr.h>
+#include <cftal/bitops.h>
 
 namespace cftal {
     namespace impl {
+        
+        uint16_t f32_to_f16(float v);
+        float f16_to_f32(uint16_t v);
+        
+        bool test_cvt_f32_f16();
+        
+        
         template <typename _T>
         std::pair<_T, _T>
         agm_step(std::pair<_T, _T> p) {
@@ -50,6 +58,85 @@ namespace cftal {
     }
 
 }
+
+cftal::uint16_t 
+cftal::impl::f32_to_f16(float v)
+{
+    uint32_t a= as<uint32_t>(v);
+    uint16_t sign = (a >> 16) & 0x8000;
+    int32_t aexp = (a >> 23) & 0xff;
+    uint32_t mantissa = a & 0x007fffff;
+    uint32_t mask;
+    uint32_t increment;
+
+    if (aexp == 0xff) {
+        if (mantissa == 0)
+            return sign | 0x7c00;   /* Infinity.  */
+        /* Remaining cases are NaNs.  Convert SNaN to QNaN.  */
+        return sign | 0x7e00 | (mantissa >> 13);
+    }
+
+    if (aexp == 0 && mantissa == 0)
+        return sign;
+
+    aexp -= 127;
+
+    /* Decimal point between bits 22 and 23.  */
+    mantissa |= 0x00800000;
+    if (aexp < -14) {
+        mask = 0x00ffffff;
+        if (aexp >= -25)
+            mask >>= 25 + aexp;
+    } else {
+        mask = 0x00001fff;
+    }
+    /* Round.  */
+    if (mantissa & mask) {
+        increment = (mask + 1) >> 1;
+        if ((mantissa & mask) == increment)
+            increment = mantissa & (increment << 1);
+        mantissa += increment;
+        if (mantissa >= 0x01000000) {
+            mantissa >>= 1;
+            aexp++;
+        }
+    }
+    if (aexp > 15)
+        return sign | 0x7c00;
+    if (aexp < -24)
+        return sign;
+    if (aexp < -14) {
+        mantissa >>= -14 - aexp;
+        aexp = -14;
+    }
+    /* We leave the leading 1 in the mantissa, and subtract one
+        from the exponent bias to compensate.  */
+    return sign | (((aexp + 14) << 10) + (mantissa >> 13));
+}
+
+float
+cftal::impl::f16_to_f32(uint16_t a)
+{
+    uint32_t sign = static_cast<uint32_t>(a & 0x8000) << 16;
+    int32_t aexp = (a >> 10) & 0x1f;
+    uint32_t mantissa = a & 0x3ff;
+
+    if (aexp == 0x1f)
+        return sign | 0x7f800000 | (mantissa << 13);
+
+    if (aexp == 0) {
+        if (mantissa == 0)
+            return sign;
+        int shift = lzcnt(mantissa) - 21;
+        mantissa <<= shift;
+        aexp = -shift;
+    }
+
+    uint32_t r= sign | (((aexp + 0x70) << 23) + (mantissa << 13));
+    return as<float>(r);
+}
+
+
 
 
 int main3(int argc, char** argv)
