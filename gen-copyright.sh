5220130f54dce03dcc92b7d3683cd29467129694
axel <axel@18305beb-b1e2-0310-bcd8-b14b5419b1d7>
2017-02-18 07:27:00 +0000

    weekly alap updates
    git-svn-id: file:///usr/local/svn/cpplib/trunk/cftal@1945 18305beb-b1e2-0310-bcd8-b14b5419b1d7

---
+++ gen-copyright.sh
@@ -0,0 +1,21 @@
+#! /bin/sh
+#set -e
+set -x
+files=`find . \( -name '*.h' -o -name '*.cc' \)`
+offset=0
+
+for i in $files
+do
+    ll=`cat $i| wc -l`
+    ll=$(($ll-$offset))
+cat >/tmp/$$ <<EOF
+//
+// Copyright Axel Zeuner 2006-2017. Use, modification and
+// distribution is subject to the Boost Software License, Version
+// 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+// 
+EOF
+    tail -n $ll $i >>/tmp/$$
+done
+#rm /tmp/$$
--- include/cftal/config.h
+++ include/cftal/config.h
@@ -1,3 +1,9 @@
+//
+// Copyright Axel Zeuner 2006-2017. Use, modification and
+// distribution is subject to the Boost Software License, Version
+// 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+// 
 #if !defined (__CFTAL_CONFIG_H__)
 #define __CFTAL_CONFIG_H__ 1
 
