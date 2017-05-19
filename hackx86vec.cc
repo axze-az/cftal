000ce75323098eb9f4559939f2b5dafa96fc3136
axel <axel@18305beb-b1e2-0310-bcd8-b14b5419b1d7>
2017-05-19 10:15:46 +0000

    more f16_t updates
    git-svn-id: file:///usr/local/svn/cpplib/trunk/cftal@1960 18305beb-b1e2-0310-bcd8-b14b5419b1d7

--- hackx86vec.cc
+++ hackx86vec.cc
@@ -16,6 +16,7 @@
 #include <cftal/bitops.h>
 #include <cftal/vec.h>
 #include <cftal/constants.h>
+#include <cftal/f16.h>
 
 namespace cftal {
 
@@ -34,34 +35,6 @@ namespace cftal {
         bool test_cvt_f16_f32();
 
     }
-
-    // a 16 bit floating point number
-    class f16_t {
-    public:
-        constexpr explicit f16_t(uint16_t f) : _f(f) {}
-        constexpr uint16_t v() const { return _f; }
-    private:
-        uint16_t _f;
-    };
-
-
-    // conversion of a f32 to a f16 value
-    f16_t
-    cvt_f32_to_f16(f32_t f);
-    // conversion of a f16 value to a f16 value
-    f32_t
-    cvt_f16_to_f32(f16_t f);
-
-
-    // conversion of a f32 vector to a f16 vector
-    template <std::size_t _N>
-    vec<uint16_t, _N>
-    f32_to_f16(const vec<float, _N>& s);
-
-    // conversion of a f16 vector to a f32 vector
-    template <std::size_t _N>
-    vec<float, _N>
-    f16_to_f32(const vec<uint16_t, _N>& s);
 
 
     namespace impl {
@@ -186,147 +159,6 @@ cftal::test::ref_f16_to_f32(uint16_t a)
     return as<float>(r);
 }
 
-
-cftal::f32_t
-cftal::cvt_f16_to_f32(f16_t t)
-{
-    uint32_t tt= t.v();
-    const uint32_t exp_msk = 0x7c00 << 13;
-    uint32_t r= (tt & 0x7fff) << 13;
-    uint32_t s= (tt & 0x8000) << 16;
-    uint32_t e= exp_msk & r;
-    r += (127-15) << 23; // adust the exponent
-    if (e == exp_msk) {
-        // maximum exponent? inf/NaNs
-        r += (128-16) << 23;
-    } else if (e == 0) {
-        // denormal numbers
-        const f32_t magic = as<f32_t>(113 << 23);
-        r += 1<<23;
-        r = as<uint32_t>(as<f32_t>(r) - magic);
-    }
-    r |= s;
-    return as<f32_t>(r);
-}
-
-cftal::f16_t
-cftal::cvt_f32_to_f16(f32_t ff)
-{
-    const f32_t inf= std::numeric_limits<float>::infinity();
-    const uint32_t inf_u= as<uint32_t>(inf);
-    const uint32_t max_f16_u = (127+16)<<23;
-    const f32_t denom_magic= 0.5f;
-
-    const uint32_t sgn_msk = 0x80000000u;
-
-    uint32_t f=as<uint32_t>(ff);
-    uint32_t s=(f & sgn_msk);
-    f ^= s;
-    s >>= 16;
-    if (f >= max_f16_u) {
-        f = f > inf_u ?
-            0x7e00 | ((f & 0x7fffff)>>13) : 0x7c00;
-    } else {
-        if (f < (113<<23)) {
-            f = as<uint32_t>(as<f32_t>(f) + denom_magic);
-            f = f - as<uint32_t>(denom_magic);
-        } else {
-            uint32_t mant_odd= (f>>13) & 1;
-            f += (uint32_t(15-127)<<23)+ 0xfff;
-            f += mant_odd;
-            f >>=13;
-        }
-    }
-    return f16_t(f|s);
-}
-
-
-
-template <std::size_t _N>
-cftal::vec<cftal::uint16_t, _N>
-cftal::f32_to_f16(const vec<float, _N> &sf)
-{
-    using uv_t = vec<uint32_t, _N>;
-    uv_t s = as<uv_t>(sf);
-    uv_t ue = (s >> exp_shift_f32) & uv_t(exp_msk_f32);
-    using sv_t = vec<int32_t, _N>;
-    sv_t e = as<sv_t>(ue) - sv_t(bias_f32);
-
-    using um_t = typename uv_t::mask_type;
-
-    // sign of result:
-    uv_t sgn= (s >> 16) & 0x8000;
-
-    // correct exp/mantissas too small for normal f16 numbers
-    // numbers with exponents smaller than 2^-25 are flushed to
-    // zero, normal numbers have an exponent above -14
-    um_t is_ftz = ue < uv_t(-25+bias_f32);
-    // this contains also nan and infs
-    um_t is_norm= ue > uv_t(-13+bias_f32);
-
-
-
-}
-
-template <std::size_t _N>
-cftal::vec<float, _N>
-cftal::f16_to_f32(const vec<uint16_t, _N> &s)
-{
-    const vec<uint16_t, _N> z(0);
-    using uv_t = vec<uint32_t, _N>;
-    uv_t sl= as<uv_t>(combine_even_odd(s, z));
-    uv_t sign = (sl << 16) & uv_t(sign_f32_msk::v.u32());
-    uv_t mant = sl & uv_t(0x3ff);
-    uv_t ue= (sl >> 10) & uv_t(0x1f);
-    // sv_t e = as<sv_t>(ue);
-
-    using um_t = typename uv_t::mask_type;
-
-    // shifted mantissa
-    uv_t mant_s = mant << 13;
-    // e  == 0x1f --> inf or NaN
-    um_t inf_nan = ue == uv_t(0x1f);
-    uv_t f_inf_nan = sign | uv_t(exp_f32_msk::v.u32()) | mant_s;
-    // default normal value:
-    uv_t f= sign | ((ue + 0x70) << exp_shift_f32) | mant_s;
-    // combine inf/nan and normal values
-    f = select(inf_nan, f_inf_nan, f);
-
-    um_t zero_or_sub_nor = ue == uv_t(0x0);
-    if (any_of(zero_or_sub_nor)) {
-        // combine zero values
-        um_t zero = um_t(mant == 0) & zero_or_sub_nor;
-        f = select(zero, sign, f);
-        // e  == 0 & mant !=0 --> subnormal
-        um_t sub_nor = um_t(mant != 0) & zero_or_sub_nor;
-        // std::cout << ue << std::endl;
-        // std::cout << mant << ' ' << sub_nor << std::endl;
-        if (any_of(sub_nor)) {
-            // the f16 hidden bit
-            const uv_t dot = uv_t(0x400);
-            // set the hidden bit for not sub normal components
-            uv_t dm= select(sub_nor, mant, dot);
-            // ue + the bias shift to reach f32 exponent with bias
-            uv_t due= ue + (0x70 + 1);
-            // shift mantissa left until we have normalized all numbers
-            // to 1.xxxx and subtract for every step one from the exponent
-            while (1) {
-                um_t is_norm = (uv_t(dm & dot) == dot);
-                if (all_of(is_norm))
-                    break;
-                dm = select(is_norm, dm, uv_t(dm << 1));
-                due= select(is_norm, due, uv_t(due - 1));
-            }
-            // clear the hidden bit
-            dm &= 0x3ff;
-            // and align the mantissa
-            dm <<=13;
-            uv_t f_sub_nor = sign | (due << exp_shift_f32) | dm;
-            f = select(sub_nor, f_sub_nor, f);
-        }
-    }
-    return as<vec<float, _N> >(f);
-}
 
 namespace cftal {
     namespace test {
@@ -532,7 +364,8 @@ int main1(int argc, char** argv)
     using namespace cftal;
     uint16_t s=32678;
     vec<uint16_t, 1> vs(s);
-    vec<float, 1> vd=f16_to_f32(vs);
+    vec<f16_t, 1> vf16=as<vec<f16_t, 1> >(vs);
+    vec<float, 1> vd=cvt_f16_to_f32(vf16);
     float ref=test::ref_f16_to_f32(s);
     std::cout << vs  << ' ' << vd << ' ' << ref << std::endl;
     return 0;
--- include/cftal/f16.h
+++ include/cftal/f16.h
@@ -9,6 +9,7 @@
 
 #include <cftal/config.h>
 #include <cftal/types.h>
+#include <cftal/constants.h>
 #include <cftal/vec.h>
 
 
@@ -30,9 +31,8 @@ namespace cftal {
     f32_t
     cvt_f16_to_f32(f16_t f);
 
-
     template <std::size_t _N>
-    vec<fp16_t, _N>
+    vec<f16_t, _N>
     cvt_f32_to_f16(arg_t<vec<f32_t, _N> > s);
 
     template <std::size_t _N>
@@ -40,15 +40,136 @@ namespace cftal {
     cvt_f16_to_f32(arg_t<vec<f16_t, _N> > s);
 }
 
+inline
+cftal::f32_t
+cftal::cvt_f16_to_f32(f16_t t)
+{
+    uint32_t tt= t.v();
+    const uint32_t exp_msk = 0x7c00 << 13;
+    uint32_t r= (tt & 0x7fff) << 13;
+    uint32_t s= (tt & 0x8000) << 16;
+    uint32_t e= exp_msk & r;
+    r += (127-15) << 23; // adust the exponent
+    if (e == exp_msk) {
+        // maximum exponent? inf/NaNs
+        r += (128-16) << 23;
+    } else if (e == 0) {
+        // denormal numbers
+        const f32_t magic = as<f32_t>(113 << 23);
+        r += 1<<23;
+        r = as<uint32_t>(as<f32_t>(r) - magic);
+    }
+    r |= s;
+    return as<f32_t>(r);
+}
+
+inline
+cftal::f16_t
+cftal::cvt_f32_to_f16(f32_t ff)
+{
+    const f32_t inf= std::numeric_limits<float>::infinity();
+    const uint32_t inf_u= as<uint32_t>(inf);
+    const uint32_t max_f16_u = (127+16)<<23;
+    const f32_t denom_magic= 0.5f;
+
+    const uint32_t sgn_msk = 0x80000000u;
+
+    uint32_t f=as<uint32_t>(ff);
+    uint32_t s=(f & sgn_msk);
+    f ^= s;
+    s >>= 16;
+    if (f >= max_f16_u) {
+        f = f > inf_u ?
+            0x7e00 | ((f & 0x7fffff)>>13) : 0x7c00;
+    } else {
+        if (f < (113<<23)) {
+            f = as<uint32_t>(as<f32_t>(f) + denom_magic);
+            f = f - as<uint32_t>(denom_magic);
+        } else {
+            uint32_t mant_odd= (f>>13) & 1;
+            f += (uint32_t(15-127)<<23)+ 0xfff;
+            f += mant_odd;
+            f >>=13;
+        }
+    }
+    return f16_t(f|s);
+}
+
+
 template <std::size_t _N>
-cftal::vec<fp16_t, _N>
+cftal::vec<cftal::f16_t, _N>
 cftal::cvt_f32_to_f16(arg_t<vec<f32_t, _N> > ff)
 {
     using f32vec = vec<f32_t, _N>;
-    using u32vec = vec<uint32_t, _N>
+    using u32vec = vec<int32_t, _N>;
 
+    const f32vec inf=std::numeric_limits<float>::infinity();
+    const u32vec inf_u= as<u32vec>(inf);
+    const u32vec max_f16_u= (bias_f32+16) << exp_shift_f32;
+    const f32vec denom_magic=0.5f;
+    const u32vec denom_magic_u=as<u32vec>(denom_magic);
+    
+    u32vec f= as<u32vec>(ff);
+    u32vec s= f & u32vec(sign_f32_msk::v.u32());
+    f ^= s;
+    // the signed shift does not matter because we truncate
+    // the 16 higher bits anyway
+    s >>= 16;
+
+    // inf or nan handling
+    auto inf_or_nan = f >= max_f16_u;
+    u32vec r_nan = u32vec(0x7e00) | ((f & sig_f32_msk::v.u32()) >> 13);
+    u32vec r_inf = u32vec(0x7c00);
+    u32vec r_inf_nan = select(f > inf_u, r_nan, r_inf);
+    // denormal handling
+    auto denom= f < u32vec(113<<23);
+    u32vec r_denom= as<u32vec>(as<f32vec>(f) + denom_magic);
+    r_denom -= denom_magic_u;
+    // normal numbers
+    u32vec mant_odd= (f>>13) & 1;
+    f += u32vec((uint32_t(15-bias_f32)<<13) +  0xfff);
+    f += mant_odd;
+    f >>= 13;
+    // produce result
+    u32vec r = select(denom, r_denom, f);
+    r = select(inf_or_nan, r_inf_nan, r);
+    r |= s;
+
+    using u16vec2 = cftal::vec<uint16_t, 2*_N>;
+    auto t=as<u16vec2>(r);
+    auto rr=even_elements(t);
+    return rr;
 }
 
+template <std::size_t _N>
+cftal::vec<cftal::f32_t, _N>
+cftal::cvt_f16_to_f32(arg_t<vec<f16_t, _N> > ff)
+{
+    using u32vec = vec<uint32_t, _N>;
+    using u16vec = vec<uint16_t, _N>;
+    using f32vec = vec<f32_t, _N>;
+    u16vec ffu16=as<u16vec>(ff);
+    u16vec z=0;
+    u32vec tt=combine_even_odd(ffu16, z);
+    
+    const u32vec exp_msk = 0x7c00 << 13;
+    u32vec r= (tt & 0x7fff) << 13;
+    u32vec s= (tt & 0x8000) << 16;
+    u32vec e= exp_msk & r;
+    r += (127-15) << 23; // adust the exponent
+
+    u32vec r_inf_nan= r + ((128-16) << 23);
+
+    u32vec r_denom = r + (1<<23);
+    const f32vec magic=as<f32_t>(113<<23);
+    r_denom = as<u32vec>(as<f32vec>(r_denom) - magic);
+
+    r = select(e== exp_msk, r_inf_nan, r);
+    r = select(e== 0, r_denom, r);
+    r |= s;
+    return as<f32vec>(r);
+}
+    
 // Local variables:
 // mode: c++
 // end:
