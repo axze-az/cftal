1ab8f2233fbab0144fc7ecf30b738361ad6bb8fb
axel <axel@18305beb-b1e2-0310-bcd8-b14b5419b1d7>
2017-06-29 06:48:19 +0000

    more updates
    git-svn-id: file:///usr/local/svn/cpplib/trunk/cftal@2103 18305beb-b1e2-0310-bcd8-b14b5419b1d7

--- test/test-pow-f64.cc
+++ test/test-pow-f64.cc
@@ -56,10 +56,18 @@ namespace cftal {
 
             using base_type::ilogbp1_k;
             using base_type::ldexp_k;
-            using base_type::pow_k;
 
             static
-            vf_type pow(arg_t<vf_type> x, arg_t<vf_type> y);
+            dvf_type
+            __pow_log(arg_t<vf_type> x);
+
+            static
+            vf_type
+            pow_k(arg_t<vf_type> x, arg_t<vf_type> y);
+
+            static
+            vf_type
+            pow(arg_t<vf_type> x, arg_t<vf_type> y);
 
         };
 
@@ -81,18 +89,6 @@ namespace cftal {
             static vf_type func(arg_t<vf_type> vf);
             static vf_type func_k(arg_t<vf_type> vf);
 
-            using base_type::ilogbp1;
-            using base_type::ldexp;
-            using base_type::__reduce_exp_arg;
-            using base_type::__scale_exp_k;
-            using base_type::__exp_k;
-
-            static
-            vf_type
-            __native_exp_k(arg_t<vf_type> xrh,
-                           arg_t<vf_type> xrl,
-                           arg_t<vf_type> kf,
-                           arg_t<vi_type> k);
         };
 
 
@@ -128,382 +124,108 @@ namespace cftal {
                 return "pow";
             }
         };
-
-
-        template <typename _T>
-        struct check_func {
-            template <std::size_t _N>
-            static
-            vec<_T, _N>
-            v(const vec<_T, _N>& a) {
-                using traits_t=math::func_traits<vec<_T, _N>,
-                                                 vec<int32_t, _N> >;
-                using func_t=math::test_func<_T, traits_t>;
-                return func_t::func(a);
-            }
-            static
-            auto
-            r(const _T& a) {
-                std::pair<_T, _T> i;
-#if EXP>0
-                _T v=call_mpfr::func(a, mpfr_exp, &i);
-#else
-                _T v=call_mpfr::func(a, mpfr_expm1, &i);
-#endif
-                return std::make_tuple(v, i.first, i.second);
-            }
-            static
-            _T
-            s(const _T& a) {
-#if EXP>0
-                return std::exp(a);
-#else
-                return std::expm1(a);
-#endif
-            }
-            static
-            const char* fname() {
-#if EXP >0
-                return "func-exp";
-#else
-                return "func-expm1";
-#endif
-            }
-        };
-
-
-
     }
-
-    // native div
-    template <typename _T>
-    _T
-    native_div(_T a, _T b);
-
-    template <typename _T, std::size_t _N>
-    vec<_T, _N>
-    native_div(const vec<_T, _N>& a, const vec<_T, _N>& b);
-
-    template <typename _T>
-    vec<_T, 1>
-    native_div(const vec<_T, 1>& a, const vec<_T, 1>& b);
-
-    v1f32
-    native_div(const v1f32& a, const v1f32& b);
-
-    v2f32
-    native_div(const v2f32& a, const v2f32& b);
-
-    v4f32
-    native_div(const v4f32& a, const v4f32& b);
-
-    v8f32
-    native_div(const v8f32& a, const v8f32& b);
 }
-
 template <typename _T>
-_T
-cftal::native_div(_T a, _T b)
-{
-    return a/b;
-}
-
-template <typename _T, std::size_t _N>
-cftal::vec<_T, _N>
-cftal::native_div(const vec<_T, _N>& a, const vec<_T, _N>& b)
-{
-    vec<_T, _N> r(native_div(low_half(a), low_half(b)),
-                  native_div(high_half(a), high_half(b)));
-    return r;
-}
-
-template <typename _T>
-cftal::vec<_T, 1>
-cftal::native_div(const vec<_T, 1>& a, const vec<_T, 1>& b)
-{
-    vec<_T, 1> r(native_div(a(), b()));
-    return r;
-}
-
 inline
-cftal::v1f32
-cftal::native_div(const v1f32& b, const v1f32& a)
+typename cftal::math::test_func<double, _T>::dvf_type
+cftal::math::test_func<double, _T>::
+__pow_log(arg_t<vf_type> xc)
 {
-#if defined (__SSE__)
-    v1f32 rcp=_mm_cvtss_f32(_mm_rcp_ss(_mm_set_ss(a())));
-    rcp = rcp + rcp*(1-rcp*a);
+    using fc = func_constants<double>;
+    vmf_type is_denom=xc <= fc::max_denormal();
+    vf_type x=_T::sel(is_denom, xc*0x1p54, xc);
+    vi2_type k=_T::sel(_T::vmf_to_vmi2(is_denom), vi2_type(-54), vi2_type(0));
+    vi2_type lx, hx;
+    _T::extract_words(lx, hx, x);
+    /* reduce x into [sqrt(2)/2, sqrt(2)] */
+    hx += 0x3ff00000 - 0x3fe6a09e;
+    k += (hx>>20) - _T::bias();
+    hx = (hx&0x000fffff) + 0x3fe6a09e;
+    vf_type xr = _T::combine_words(lx, hx);
+
+    // vf_type f = xr - 1.0;
+    dvf_type df= d_ops::add(xr, vf_type(-1.0));
+    // vf_type f= df.h();
+    dvf_type dhfsq = mul_pwr2(sqr(df), vf_type(0.5));
+    dvf_type ds=d_ops::sloppy_div(df, (vf_type(2.0)+df));
+    // with mul12(zh, zl, ds.h(), ds.h()) pow does not become
+    // faitfully rounded
+    dvf_type dz=sqr(ds);
+    vf_type zh=dz.h(); //, zl=dz.l();
+    // d_ops::mul22(zh, zl, ds.h(), ds.l(), ds.h(), ds.l());
+    // x^2 : +0xa.aaaaaaaaaaaa8p-4
+    const vf_type log_c2=+6.6666666666666662965923e-01;
+    // x^4 : +0xc.ccccccccceac8p-5
+    const vf_type log_c4=+4.0000000000005325739849e-01;
+    // x^6 : +0x9.24924921770b8p-5
+    const vf_type log_c6=+2.8571428569168361155661e-01;
+    // x^8 : +0xe.38e392e2fd49p-6
+    const vf_type log_c8=+2.2222222656633455750708e-01;
+    // x^10 : +0xb.a2e6dba5baafp-6
+    const vf_type log_c10=+1.8181773614825663676342e-01;
+    // x^12 : +0x9.d90aca733a8d8p-6
+    const vf_type log_c12=+1.5387220163840073516859e-01;
+    // x^14 : +0x8.7a5091423103p-6
+    const vf_type log_c14=+1.3246549783140454392694e-01;
+    // x^16 : +0x8.811edd68c1cb8p-6
+    const vf_type log_c16=+1.3288089391701582431260e-01;
 #if 1
-    return b*rcp;
+    vf_type w= zh*zh;
+    vf_type ey= horner(w,
+                       log_c16,
+                       log_c12,
+                       log_c8);
+    vf_type oy= horner(w,
+                       log_c14,
+                       log_c10,
+                       log_c6);
+    vf_type y= horner(zh, ey, oy, log_c4);
 #else
-    // goldstein iteration
-    v1f32 x= b*rcp;
-    v1f32 y= a*rcp;
-    v1f32 r= 2-y;
-    y= r*y;
-    v1f32 bda= r*x;
-    return bda;
+    // vf_type w = z*z;
+    vf_type y= horner(zh,
+                      log_c16,
+                      log_c14,
+                      log_c12,
+                      log_c10,
+                      log_c8,
+                      log_c6,
+                      log_c4);
 #endif
-#else
-    return x/y;
-#endif
-}
+    // vf_type ye;
+    // horner_comp_quick(y, ye, zh, y, log_c2);
+    y = zh*y + log_c2;
+    // d_ops::mul12(y, ye, zh, y);
+    y *= zh;
+    using ctbl=impl::d_real_constants<d_real<double>, double>;
+    vf_type kf = _T::cvt_i_to_f(_T::vi2_odd_to_vi(k));
 
-inline
-cftal::v2f32
-cftal::native_div(const v2f32& x, const v2f32& y)
-{
-#if defined (__SSE__)
-    v4f32 x4(x, x);
-    v4f32 y4(y, y);
-    return low_half(native_div(x4, y4));
-#else
-    return x/y;
-#endif
-}
-
-inline
-cftal::v4f32
-cftal::native_div(const v4f32& b, const v4f32& a)
-{
-#if defined (__SSE__)
-    v4f32 rcp=_mm_rcp_ps(a());
-    rcp = rcp + rcp*(1-rcp*a);
-#if 1
-    return b*rcp;
-#else
-    // goldstein iteration
-    v4f32 x= b*rcp;
-    v4f32 y= a*rcp;
-    v4f32 r= 2-y;
-    y= r*y;
-    v4f32 bda= r*x;
-    return bda;
-#endif
-#else
-    return x/y;
-#endif
-}
-
-inline
-cftal::v8f32
-cftal::native_div(const v8f32& b, const v8f32& a)
-{
-#if defined (__AVX__)
-    v8f32 rcp=_mm256_rcp_ps(a());
-    rcp = rcp + rcp*(1-rcp*a);
-#if 1
-    return b*rcp;
-#else
-    // goldstein iteration
-    v8f32 x= b*rcp;
-    v8f32 y= a*rcp;
-    v8f32 r= 2-y;
-    y= r*y;
-    v8f32 bda= r*x;
-    return bda;
-#endif
-#else
-    return a/b;
-#endif
+    dvf_type log_x= // kf* dvf_type(ctbl::m_ln2);
+        vf_type(kf* ctbl::m_ln2_cw[1]);
+    log_x += vf_type(kf*ctbl::m_ln2_cw[0]);
+    // log_x += ds*dhfsq;
+    // log_x += ds*dvf_type(y, ye);
+    // combine these two factors to save one mul22:
+    dvf_type dhfsq_y= dhfsq + y; // dvf_type(y,ye);
+    log_x += ds*dhfsq_y;
+    log_x += (df -dhfsq);
+    return log_x;
 }
 
 template <typename _T>
 inline
-typename cftal::math::test_func<float, _T>::vf_type
-cftal::math::test_func<float, _T>::
-__native_exp_k(arg_t<vf_type> xrh,
-               arg_t<vf_type> xrl,
-               arg_t<vf_type> kf,
-               arg_t<vi_type> k)
+typename cftal::math::test_func<double, _T>::vf_type
+cftal::math::test_func<double, _T>::pow_k(arg_t<vf_type> x,
+                                          arg_t<vf_type> y)
 {
-#if 0
-#if 0
-    // [0, 0.3465735912322998046875] : | p - f | <= 2^-27.578125
-    // coefficients for native_exp generated by sollya
-    // x^0 : +0xa.aaaabp-6f
-    const float native_exp_c0=+1.6666667163e-01f;
-    // x^2 : -0xb.60dfap-12f
-    const float native_exp_c2=-2.7779326774e-03f;
-    // x^4 : +0x8.c6d66p-17f
-    const float native_exp_c4=+7.0000000000e-05f;
-    vf_type xx= xrh*xrh;
-    vf_type P= impl::poly(xx,
-                          native_exp_c4,
-                          native_exp_c2,
-                          native_exp_c0);
-    vf_type br= xrh- xx*P;
-#if 0
-    vf_type t= br/(2.0f-br);
-    vf_type y, ye;
-    d_ops::add12(y, ye, vf_type(1.0), t);
-    vf_type yl=y+ye;
-    impl::eft_poly_si(y, ye, xrh, y, ye, vf_type(1.0));
-    ye += xrl + xrl*xrh*yl;
-    y += ye;
-#else
-    // y += (xrh*br)/(2.0f-br) + xrh;
-    vf_type t= (xrh*br)/(2.0f-br) + xrh;
-    vf_type y = xrl + xrl*t;
-    y += t;
-    y += 1.0f;
-#endif
-    y = __scale_exp_k(y, kf, k);
-    return y;
-#else
-    // exp(x) = 1 + 2x/(2-x+x^2*P(x^2)
-    //                    x [x - x^2 * P]
-    //        = 1 + x + ------------------
-    //                    2 - [x - x^2 * P]
-    // x^2 : +0xa.aaa8fp-6f
-#if 0
-    // pade 4, 4, taylor 8 of %e^x-1-x faithfully
-    vf_type p= horner(xrh,
-                      +60.0f,
-                      -280.0f,
-                      +4200.0f)*xrh*xrh;
-    vf_type q= horner1(xrh,
-                       // 1.0f
-                       -40.0f,
-                       +540.0f,
-                       -3360.0f,
-                       +8400.0f);
-    vf_type y= xrh + p/q;
-    vf_type ye= xrl + xrl*xrh;
-    y += ye;
-    y += 1.0f;
-    y = __scale_exp_k(y, kf, k);
-    return y;
-#endif
-#if 1
-
-    vf_type e= horner(xrh,
-                      7.610925536017418e1f,
-                      8.73744877496524e2f,
-                      4.256882124716833e3f,
-                      8.368816062267611e3f);
-    vf_type d= horner(xrh,
-                      -6.4e1f,
-                      8.012710646668774e2f,
-                      -4.111933953502037e3f,
-                      8.36881606265761e3f);
-#if 0
-    vmf_type x_lt_z = xrh < vf_type(0.0);
-    vf_type p= _T::sel(x_lt_z, d, e);
-    vf_type q= _T::sel(x_lt_z, e, d);
-    vf_type y= p/q;
-#else
-    vf_type y= e/d;
-#endif
-    vf_type ye= xrl + xrl*xrh;
-    y += ye;
-    y = __scale_exp_k(y, kf, k);
-    return y;
-#endif
-#endif
-#else
-    // [-0.3465735912322998046875, 0.3465735912322998046875] : | p - f | <= 2^-32.609375
-    // coefficients for exp generated by sollya
-    // x^0 : +0x8p-3f
-    const float exp_c0=+1.0000000000e+00f;
-    // x^1 : +0x8p-3f
-    const float exp_c1=+1.0000000000e+00f;
-    // x^2 : +0x8p-4f
-    const float exp_c2=+5.0000000000e-01f;
-    // x^3 : +0xa.aaaa9p-6f
-    const float exp_c3=+1.6666664183e-01f;
-    // x^4 : +0xa.aaa73p-8f
-    const float exp_c4=+4.1666459292e-02f;
-    // x^5 : +0x8.88ae5p-10f
-    const float exp_c5=+8.3338962868e-03f;
-    // x^6 : +0xb.6a77ep-13f
-    const float exp_c6=+1.3935414609e-03f;
-    // x^7 : +0xc.d3317p-16f
-    const float exp_c7=+1.9569355936e-04f;
-#if 1
-    vf_type y= impl::estrin(xrh,
-                            exp_c7,
-                            exp_c6,
-                            exp_c5,
-                            exp_c4,
-                            exp_c3,
-                            exp_c2);
-#else
-    vf_type xx= xrh*xrh;
-    vf_type i=horner(xx,
-                     exp_c7,
-                     exp_c5);
-    vf_type j=horner(xx,
-                     exp_c6,
-                     exp_c4);
-    vf_type y= horner(xrh, i, j, exp_c3, exp_c2);
-#endif
-    vf_type ye;
-    horner_comp_quick(y, ye, xrh, y, exp_c1);
-    // correction for errors in argument reduction
-    vf_type yl=y +ye;
-    horner_comp_quick_si(y, ye, xrh, y, ye, exp_c0);
-    vf_type yee= xrl + xrl*xrh*yl;
-    ye += yee;
-    y += ye;
-    y = __scale_exp_k(y, kf, k);
-    return y;
-#endif
-}
-
-template <typename _T>
-inline
-typename cftal::math::test_func<float, _T>::vf_type
-cftal::math::test_func<float, _T>::func_k(arg_t<vf_type> xc)
-{
+    vf_type abs_x= abs(x);
+    dvf_type lnx = __pow_log(abs_x);
+    dvf_type ylnx = lnx * y;
     vf_type xrh, xrl, kf;
-    auto k= __reduce_exp_arg(xrh, xrl, kf, xc);
-#if EXP> 0
-    auto y= __native_exp_k(xrh, xrl, kf, k);
-#else
-    // exp(x) = 1 + 2x/(2-x+x^2*P(x^2)
-    //                    x [x - x^2 * P]
-    //        = 1 + x + ------------------
-    //                    2 - [x - x^2 * P]
-    // [0, 0.3465735912322998046875] : | p - f | <= 2^-27.578125
-    // coefficients for native_exp generated by sollya
-    // x^0 : +0xa.aaaabp-6f
-    const float native_exp_c0=+1.6666667163e-01f;
-    // x^2 : -0xb.60dfap-12f
-    const float native_exp_c2=-2.7779326774e-03f;
-    // x^4 : +0x8.c6d66p-17f
-    const float native_exp_c4=+7.0000000000e-05f;
-    vf_type xx= xrh*xrh;
-    vf_type P= impl::poly(xx,
-                          native_exp_c4,
-                          native_exp_c2,
-                          native_exp_c0);
-    vf_type br= xrh- xx*P;
-    vf_type t= br/(2.0f-br);
-    vf_type y, ye;
-    d_ops::add12(y, ye, vf_type(1.0), t);
-    impl::eft_poly_si(y, ye, xrh, y, ye, vf_type(1.0));
-    ye += xrl + xrl*xrh;
-    // 2^kf = 2*2^s ; s = kf/2
-    vf_type scale = __scale_exp_k(vf_type(0.5f), kf, k);
-    // e^x-1 = 2*(y * 2^s - 0.5)
-    impl::eft_poly_si(y, ye, scale, y, ye, vf_type(-0.5f));
-    y *= 2;
-    y  = y + 2*ye;
-    // x small, required for handling of subnormal numbers
-    y = _T::sel((abs(xrh) < 0x1p-25f) & (kf==0.0), xrh, y);
-    return y;
-#endif
-    return y;
-}
-
-template <typename _T>
-typename cftal::math::test_func<float, _T>::vf_type
-cftal::math::test_func<float, _T>::
-func(arg_t<vf_type> d)
-{
-#if EXP
-    vf_type res=func_k(d);
-    using fc= func_constants<float>;
+    auto k=base_type::__reduce_exp_arg(xrh, xrl, kf, ylnx.h(), ylnx.l());
+    vf_type res=base_type:: template __exp_k<false>(xrh, xrl, kf, k);
+    using fc=func_constants<double>;
+    const vf_type& d= ylnx.h();
     const vf_type exp_hi_inf= fc::exp_hi_inf();
     const vf_type exp_lo_zero= fc::exp_lo_zero();
     res = _T::sel(d <= exp_lo_zero, 0.0, res);
@@ -511,65 +233,7 @@ func(arg_t<vf_type> d)
     res = _T::sel(d == 0.0, 1.0, res);
     res = _T::sel(d == 1.0, M_E, res);
     return res;
-#else
-    vf_type res = func_k(d);
-    using fc= func_constants<float>;
-    const vf_type expm1_hi_inf= fc::expm1_hi_inf();
-    const vf_type expm1_lo_minus_one= fc::expm1_lo_minus_one();
-    res = _T::sel(d <= expm1_lo_minus_one, -1.0, res);
-    res = _T::sel(d >= expm1_hi_inf, _T::pinf(), res);
-    res = _T::sel(d == 0.0, 0.0, res);
-    res = _T::sel(d == 1.0, M_E-1.0, res);
-    return res;
-#endif
 }
-
-#if 0
-int main(int argc, char** argv)
-{
-    using namespace cftal::test;
-    std::cout << std::setprecision(18) << std::scientific;
-    std::cerr << std::setprecision(18) << std::scientific;
-    const int ulp=1;
-    const int _N=16;
-    bool rc=true;
-    bool speed_only=false;
-    std::size_t cnt=update_cnt(0x8000);
-    if ((argc > 1) && (std::string(argv[1]) == "--speed")) {
-        speed_only=true;
-        cnt *=8;
-    }
-    using ftype = float;
-
-    // func_domain<double> d=std::make_pair(0.0,
-    //                                     std::numeric_limits< double >::max());
-    func_domain<ftype> d=std::make_pair(-std::numeric_limits<ftype>::max(),
-                                         std::numeric_limits<ftype>::max());
-#if EXP>0
-    d=std::make_pair(-104.0f, 89.0f);
-#else
-    d=std::make_pair(-18.0f, 89.0f);
-    // d=std::make_pair(-M_LN2/2, M_LN2/2);
-#endif
-    exec_stats st(_N);
-    auto us=std::make_shared<ulp_stats>();
-    rc &= of_fp_func_up_to<
-        ftype, _N, check_func<ftype> >::v(st, d, speed_only,
-                                          cmp_ulp<ftype>(ulp, us),
-                                          cnt);
-#if 0
-    d=std::make_pair(-0x1p-4, 0x1p-4);
-    rc &= of_fp_func_up_to<
-        ftype, _N, check_func<ftype> >::v(st, d, speed_only,
-                                           cmp_ulp<ftype>(ulp, us),
-                                           cnt>>2);
-#endif
-    std::cout << "ulps: "
-              << std::fixed << std::setprecision(4) << *us << std::endl;
-    std::cout << st << std::endl;
-    return (rc == true) ? 0 : 1;
-}
-#endif
 
 template <typename _T>
 inline
@@ -652,11 +316,9 @@ cftal::math::test_func<double, _T>::pow(arg_t<vf_type> x,
 int main(int argc, char** argv)
 {
     using namespace cftal::test;
-#if 0
-    return 0;
-#else
+
     const int ulp=64;
-    const int _N=16;
+    const int _N=4;
     bool rc=true;
     bool speed_only=false;
     std::cout << std::setprecision(18) << std::scientific;
@@ -666,7 +328,7 @@ int main(int argc, char** argv)
         speed_only=true;
         cnt *=8;
     } else {
-#if 1
+#if 0
         std::string test_data_dir = dirname(argv[0]);
         std::string test_data_file=
             append_filename(test_data_dir, "../../test/data/pow.testdata");
@@ -690,11 +352,10 @@ int main(int argc, char** argv)
     rc &= of_fp_func_2_up_to<
         double, _N, check_pow<double> >::v(st, d, d, speed_only,
                                            cmp_ulp<double>(ulp, us),
-                                           cnt>>2, false);
+                                           cnt>>2, true);
     std::cout << "ulps: "
               << std::fixed << std::setprecision(4) << *us << std::endl;
     std::cout << st << std::endl;
-
     st=exec_stats(_N);
     us=std::make_shared<ulp_stats>();
     func_domain<double> d1=std::make_pair(0.0, 20.0);
@@ -722,5 +383,4 @@ int main(int argc, char** argv)
               << std::fixed << std::setprecision(4) << *us << std::endl;
     std::cout << st << std::endl;
     return (rc == true) ? 0 : 1;
-#endif
 }
