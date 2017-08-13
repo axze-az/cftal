2ae3b7064e9b2d53528612d400f7392ac1c67b3b
axel <axel@18305beb-b1e2-0310-bcd8-b14b5419b1d7>
2017-08-13 11:00:48 +0000

    use division instead of newton raphson iteration for division
    git-svn-id: file:///usr/local/svn/cpplib/trunk/cftal@2257 18305beb-b1e2-0310-bcd8-b14b5419b1d7

--- include/cftal/d_real.h
+++ include/cftal/d_real.h
@@ -1499,7 +1499,7 @@ rcp2(_T& rh, _T& rl, const _T& ah, const _T& al)
     _T th, tl;
     // -rcp * a
     mul122(th, tl, -r0h, ah, al);
-    // 1 - rcp * a
+    // 2 - rcp * a
     add122(th, tl, _T(2.0), th, tl);
     // rcp ( 2 - rcp*a)
     mul122(rh, rl, r0h, th, tl);
