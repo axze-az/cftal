d5d4434e1b4621b35d55e410699c5c921e130e88
axel <axel@18305beb-b1e2-0310-bcd8-b14b5419b1d7>
2017-12-25 08:52:37 +0000

    correction of the conversion routines
    git-svn-id: file:///usr/local/svn/cpplib/trunk/cftal@2619 18305beb-b1e2-0310-bcd8-b14b5419b1d7

--- include/cftal/vec_cvt.h
+++ include/cftal/vec_cvt.h
@@ -378,11 +378,8 @@ namespace cftal {
             static
             vec<int32_t, 1>
             l(const vec<double, 1>& s) {
-                vec<int32_t, 1> r(
-                    static_cast<int32_t>(s() < 0 ? s() - 0.5 : s() + 0.5));
-                return r;
-                // return vec<int32_t, 1>(
-                //    static_cast<int32_t>(std::rint(s())));
+                return vec<int32_t, 1>(
+                    static_cast<int32_t>(std::rint(s())));
             }
             static
             vec<int32_t, 1>
