3610abc588f9c94505e76ff30ed8caacf39dd091
axel <axel@18305beb-b1e2-0310-bcd8-b14b5419b1d7>
2017-12-31 08:00:09 +0000

    adapt the copyright
    git-svn-id: file:///usr/local/svn/cpplib/trunk/cftal@2634 18305beb-b1e2-0310-bcd8-b14b5419b1d7

--- gen-copyright.sh
+++ gen-copyright.sh
@@ -2,7 +2,7 @@
 #set -e
 set -x
 files=`find . \( -name '*.h' -o -name '*.cc' \)`
-offset=0
+offset=6
 
 for i in $files
 do
@@ -10,7 +10,7 @@ do
     ll=$(($ll-$offset))
 cat >/tmp/$$ <<EOF
 //
-// Copyright Axel Zeuner 2010-2017. Use, modification and
+// Copyright Axel Zeuner 2010-2018. Use, modification and
 // distribution is subject to the Boost Software License, Version
 // 1.0. (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
