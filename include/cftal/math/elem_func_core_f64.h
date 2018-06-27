91b41d634686650decd6f5c0acc3bf04454bc8c3
axel <axel@18305beb-b1e2-0310-bcd8-b14b5419b1d7>
2018-06-27 09:04:08 +0000

    more tests
    git-svn-id: file:///usr/local/svn/cpplib/trunk/cftal@2788 18305beb-b1e2-0310-bcd8-b14b5419b1d7

--- include/cftal/math/elem_func_core_f64.h
+++ include/cftal/math/elem_func_core_f64.h
@@ -1035,14 +1035,14 @@ __exp_k(arg_t<vf_type> xrh, arg_t<vf_type> xrl,
     const double exp_c13=+1.6594686274338619941159e-10;
     vf_type x2=xrh*xrh;
 
-    static const double oc[]={
+    static const vftype oc[]={
         exp_c13,
         exp_c11,
         exp_c9,
         exp_c7,
         exp_c5
     };
-    static const double ec[]= {
+    static const vftype ec[]= {
         exp_c12,
         exp_c10,
         exp_c8,
