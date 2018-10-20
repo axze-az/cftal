514042f476429180ebaa7e795ded972cbfcc2106
axel <axel@18305beb-b1e2-0310-bcd8-b14b5419b1d7>
2018-10-20 07:15:50 +0000

    removal of unused code
    git-svn-id: file:///usr/local/svn/cpplib/trunk/cftal@3072 18305beb-b1e2-0310-bcd8-b14b5419b1d7

--- src/lanczos.cc
+++ src/lanczos.cc
@@ -7,26 +7,6 @@
 #include "cftal/math/lanczos.h"
 
 constexpr const double
-cftal::math::lanczos_table_boost_g_6_0246_N13::p[];
-constexpr const double
-cftal::math::lanczos_table_boost_g_6_0246_N13::q[];
-
-constexpr const double
-cftal::math::lanczos_table_g_6_02468_N13::p[];
-constexpr const double
-cftal::math::lanczos_table_g_6_02468_N13::q[];
-
-constexpr const cftal::d_real<double>
-cftal::math::lanczos_table_g_6_02468_N13::pd[];
-constexpr const cftal::d_real<double>
-cftal::math::lanczos_table_g_6_02468_N13::qd[];
-
-constexpr const float
-cftal::math::lanczos_table_g_5_00000_N7::p[];
-constexpr const float
-cftal::math::lanczos_table_g_5_00000_N7::q[];
-
-constexpr const double
 cftal::math::lanczos_table_g_12_06815_N12::p[];
 constexpr const double
 cftal::math::lanczos_table_g_12_06815_N12::q[];
