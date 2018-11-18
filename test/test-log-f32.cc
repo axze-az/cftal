eb824814719115ce2fe8e9a77283b67829ad6804
axel <axel@18305beb-b1e2-0310-bcd8-b14b5419b1d7>
2018-11-18 10:37:42 +0000

    f32 log conversion
    git-svn-id: file:///usr/local/svn/cpplib/trunk/cftal@3164 18305beb-b1e2-0310-bcd8-b14b5419b1d7

--- test/test-log-f32.cc
+++ test/test-log-f32.cc
@@ -4,13 +4,27 @@
 // 1.0. (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 //
-#include "cftal/test/of_math_funcs.h"
+#include "cftal/test/program.h"
 #include "cftal/test/check_log.h"
 #include <iostream>
 #include <iomanip>
 
 int main(int argc, char** argv)
 {
+#if 1
+    using namespace cftal::test;
+    const func_domain<float> di[]={
+        std::make_pair(-0.0001f,
+                       std::numeric_limits< float >::max())
+    };
+    const int shifts[]={0};
+    int r=program<check_log<float>, 16, 1, 0x8000>(argc,
+                                                   argv,
+                                                   mpfr_log,
+                                                   di,
+                                                   shifts);
+    return r;
+#else
     using namespace cftal::test;
     const int _N=16;
     const int ulp=1;
@@ -33,4 +47,5 @@ int main(int argc, char** argv)
               << std::fixed << std::setprecision(4) << *us << std::endl;
     std::cout << st << std::endl;
     return (rc == true) ? 0 : 1;
+#endif
 }
--- test/test-log1p-f32.cc
+++ test/test-log1p-f32.cc
@@ -4,13 +4,26 @@
 // 1.0. (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 //
-#include "cftal/test/of_math_funcs.h"
+#include "cftal/test/program.h"
 #include "cftal/test/check_log1p.h"
-#include <iostream>
-#include <iomanip>
 
 int main(int argc, char** argv)
 {
+#if 1
+    using namespace cftal::test;
+
+    const func_domain<float> di[]={
+        std::make_pair(-1.0001f,
+                       std::numeric_limits< float >::max())
+    };
+    const int shifts[]={0};
+    int r=program<check_log1p<float>, 16, 1, 0x8000>(argc,
+                                                     argv,
+                                                     mpfr_log1p,
+                                                     di,
+                                                     shifts);
+    return r;
+#else
     using namespace cftal::test;
     std::cout << std::setprecision(18) << std::scientific;
     std::cerr << std::setprecision(18) << std::scientific;
@@ -35,4 +48,5 @@ int main(int argc, char** argv)
               << std::fixed << std::setprecision(4) << *us << std::endl;
     std::cout << st << std::endl;
     return (rc == true) ? 0 : 1;
+#endif
 }
