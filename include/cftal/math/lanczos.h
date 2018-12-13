d5bf97045c4a0bb115f41a61331043175563fa32
axel <axel@18305beb-b1e2-0310-bcd8-b14b5419b1d7>
2018-12-13 15:28:50 +0000

    faster calculation of the lanczos rational
    git-svn-id: file:///usr/local/svn/cpplib/trunk/cftal@3257 18305beb-b1e2-0310-bcd8-b14b5419b1d7

--- include/cftal/math/lanczos.h
+++ include/cftal/math/lanczos.h
@@ -313,8 +313,8 @@ lanczos_rational_at(const _T& x,
         // compensated horner step with higher precision coefficients
         _T p_i, o_i;
         d_ops::mul12(ph, p_i, ph, x);
-        d_ops::add122cond(ph, o_i, ph, pp[i][0], pp[i][1]);
-        pl = pl*x + (p_i + o_i);
+        d_ops::add12cond(ph, o_i, ph, pp[i][0]);
+        pl = pl*x + (p_i + o_i + pp[i][1]);
 #else
         d_ops::mul122(ph, pl, x, ph, pl);
         d_ops::add22cond(ph, pl,
