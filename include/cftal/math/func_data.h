37d4b50af5b6fdff10f58b41700fbf259e265ca7
axel <axel@18305beb-b1e2-0310-bcd8-b14b5419b1d7>
2019-01-27 10:32:02 +0000

    addition of a missing header
    git-svn-id: file:///usr/local/svn/cpplib/trunk/cftal@3351 18305beb-b1e2-0310-bcd8-b14b5419b1d7

---
+++ include/cftal/math/func_data.h
@@ -0,0 +1,41 @@
+//
+// Copyright Axel Zeuner 2010-2019. Use, modification and
+// distribution is subject to the Boost Software License, Version
+// 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+#if !defined (__CFTAL_MATH_FUNC_DATA_H__)
+#define __CFTAL_MATH_FUNC_DATA_H__ 1
+
+#include <cftal/config.h>
+
+namespace cftal {
+    namespace math {
+
+        // tables for exponential function
+        template <typename _F>
+        struct exp_data {
+            enum {
+                EXP_N=32
+            };
+            struct table_t {
+                const _F _exp_fxi_h_tbl[EXP_N];
+                const _F _exp_fxi_l_tbl[EXP_N];
+            };
+            static
+            const table_t _tbl;
+        };
+
+        extern template
+        struct exp_data<float>;
+
+        extern template
+        struct exp_data<double>;
+
+    }
+}
+
+// Local Variables:
+// mode: c++
+// end:
+#endif
