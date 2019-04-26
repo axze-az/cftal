2807ed6aa39699749ec477d7ccafdf4213df66c9
axel <axel@18305beb-b1e2-0310-bcd8-b14b5419b1d7>
2019-04-26 16:20:35 +0000

    round_to_odd implementation
    git-svn-id: file:///usr/local/svn/cpplib/trunk/cftal@3616 18305beb-b1e2-0310-bcd8-b14b5419b1d7

--- include/cftal/vec_math_funcs.h
+++ include/cftal/vec_math_funcs.h
@@ -9,10 +9,18 @@
 
 #include <cftal/config.h>
 #include <cftal/types.h>
-#include <cftal/vec_t_n.h>
+#include <cftal/vec_spec.h>
 
 namespace cftal {
+
     namespace impl {
+
+        // round to odd implementation, _F floating point type
+        // _I integer type with the same size,
+        // _FM mask type of _F, _IM mask type of _I
+        template <typename _F, typename _FM, typename _I, typename _IM>
+        _F
+        round_to_odd(const _F& xh, const _F& xl);
 
         template <typename _T, std::size_t _N>
         struct is_vec_math_func {
@@ -60,6 +68,27 @@ namespace cftal {
         };
 
     }
+}
+
+template <typename _F, typename _FM, typename _I, typename _IM>
+_F
+cftal::impl::round_to_odd(const _F& xh, const _F& xl)
+{
+    _I ih=as<_I>(xh);
+    // select rounding direction in dependence of the sign of xh, xl
+    _I ihp= ih + 1;
+    _I ihm= ih - 1;
+    _FM msk= (xl > _F(0.0)) ^ (xh < _F(0.0));
+    _IM imsk=as<_IM>(msk);
+    _I ir0 = select(msk, ihp, ihm);
+    // check if xh is already odd
+    _IM i_xh_odd= (ih & 1) != _I(0);
+    _FM xh_odd= as<_FM>(i_xh_odd);
+    _F r0= as<_I>(ir0);
+    // check if xl zero
+    _FM xl_zero_or_xh_odd= (xl == 0.0) | (xh_odd);
+    _F r= select(xl_zero_or_xh_odd, xh, r0);
+    return r;
 }
 
 // Local variables:
