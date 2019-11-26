ba2463c06f0024b7c249345fd65f69bf92397269
axel <axel@18305beb-b1e2-0310-bcd8-b14b5419b1d7>
2019-11-26 13:13:29 +0000

    next attempt
    git-svn-id: file:///usr/local/svn/cpplib/trunk/cftal@3933 18305beb-b1e2-0310-bcd8-b14b5419b1d7

--- include/cftal/vec_float_n.h
+++ include/cftal/vec_float_n.h
@@ -33,9 +33,9 @@
 #define V4F32_SLOW_SPEC_FUNCS                                           \
     ((V4F32_SPECIALIZED>0) || ((V2F32_SPECIALIZED>0) && (V2S32_SPECIALIZED>0)))
 #define V8F32_SLOW_SPEC_FUNCS                                           \
-    ((V8F32_SPECIALIZED>0))
-#define V16F32_SLOW_SPEC_FUNCS                                           \
     ((V8F32_SPECIALIZED>0) || ((V4F32_SPECIALIZED>0) && (V4S32_SPECIALIZED>0)))
+#define V16F32_SLOW_SPEC_FUNCS                                           \
+    ((V16F32_SPECIALIZED>0) || ((V8F32_SPECIALIZED>0) && (V8S32_SPECIALIZED>0)))
 
 namespace cftal {
 
