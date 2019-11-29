423635443506b64569db3901b7610ee0646ebc67
axel <axel@18305beb-b1e2-0310-bcd8-b14b5419b1d7>
2019-11-29 15:33:12 +0000

    disable the slow special functions even on fast machines
    git-svn-id: file:///usr/local/svn/cpplib/trunk/cftal@3954 18305beb-b1e2-0310-bcd8-b14b5419b1d7

--- include/cftal/vec_double_n.h
+++ include/cftal/vec_double_n.h
@@ -27,9 +27,9 @@
 #endif
 
 #define V4F64_SLOW_SPEC_FUNCS                                           \
-    ((V4F64_SPECIALIZED>0) || ((V2F64_SPECIALIZED>0) && (V4S32_SPECIALIZED>0)))
+    ((V4F64_SPECIALIZED>0))
 #define V8F64_SLOW_SPEC_FUNCS                                           \
-    ((V8F64_SPECIALIZED>0) || ((V4F64_SPECIALIZED>0) && (V8S32_SPECIALIZED>0)))
+    ((V8F64_SPECIALIZED>0))
 
 namespace cftal {
 
