dc537521e3713da7af9969588a79828eb71e529d
axel <axel@18305beb-b1e2-0310-bcd8-b14b5419b1d7>
2019-12-17 15:23:57 +0000

    definitions for lanczos tables
    git-svn-id: file:///usr/local/svn/cpplib/trunk/cftal@4082 18305beb-b1e2-0310-bcd8-b14b5419b1d7

--- src/lanczos.cc
+++ src/lanczos.cc
@@ -37,3 +37,29 @@ constexpr const cftal::d_real<float>
 cftal::math::lanczos_table_g_5_59172_N6::pedf[];
 constexpr const float
 cftal::math::lanczos_table_g_5_59172_N6::qf[];
+
+constexpr const double
+cftal::math::lanczos_table_g_10_90990_N11::p[];
+constexpr const double
+cftal::math::lanczos_table_g_10_90990_N11::q[];
+constexpr const cftal::d_real<double>
+cftal::math::lanczos_table_g_10_90990_N11::pd[];
+constexpr const cftal::d_real<double>
+cftal::math::lanczos_table_g_10_90990_N11::qd[];
+constexpr const cftal::d_real<double>
+cftal::math::lanczos_table_g_10_90990_N11::ped[];
+
+constexpr const double
+cftal::math::lanczos_table_g_4_35169_N5::p[];
+constexpr const double
+cftal::math::lanczos_table_g_4_35169_N5::q[];
+constexpr const cftal::d_real<double>
+cftal::math::lanczos_table_g_4_35169_N5::pd[];
+constexpr const cftal::d_real<double>
+cftal::math::lanczos_table_g_4_35169_N5::qd[];
+constexpr const cftal::d_real<float>
+cftal::math::lanczos_table_g_4_35169_N5::pdf[];
+constexpr const cftal::d_real<float>
+cftal::math::lanczos_table_g_4_35169_N5::pedf[];
+constexpr const float
+cftal::math::lanczos_table_g_4_35169_N5::qf[];
