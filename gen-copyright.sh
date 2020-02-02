f0e34a69eaad8df265bb068cef99391c45661ac7
axel <axel@18305beb-b1e2-0310-bcd8-b14b5419b1d7>
2020-02-02 06:12:52 +0000

    copyright update
    git-svn-id: file:///usr/local/svn/cpplib/trunk/cftal@4238 18305beb-b1e2-0310-bcd8-b14b5419b1d7

--- gen-copyright.sh
+++ gen-copyright.sh
@@ -10,7 +10,7 @@ do
     ll=$(($ll-$offset))
 cat >/tmp/$$ <<EOF
 //
-// Copyright Axel Zeuner 2010-2019. Use, modification and
+// Copyright Axel Zeuner 2010-2020. Use, modification and
 // distribution is subject to the Boost Software License, Version
 // 1.0. (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
