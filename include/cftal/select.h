aeb423ab22c23f6afac121a895de382603de4867
axel <axel@18305beb-b1e2-0310-bcd8-b14b5419b1d7>
2021-05-18 09:11:13 +0000

    the type of first parameter of select should be bool
    git-svn-id: file:///usr/local/svn/cpplib/trunk/cftal@4521 18305beb-b1e2-0310-bcd8-b14b5419b1d7

--- include/cftal/select.h
+++ include/cftal/select.h
@@ -12,23 +12,23 @@
 namespace cftal {
 
     // select: m ? on_true : on_false
-    template <class _M, class _T>
-    _T select(const _M& m,
+    template <class _T>
+    _T select(bool m,
               const _T& on_true, const _T& on_false);
 
     // if_else: m ? on_true : on_false
-    template <class _M, class _T>
-    _T if_else(const _M& m,
+    template <class _T>
+    _T if_else(bool m,
                const _T& on_true, const _T& on_false);
 
     // select_val_or_zero: m ? on_true : _T(0)
-    template <class _M, class _T>
-    _T select_val_or_zero(const _M& m,
+    template <class _T>
+    _T select_val_or_zero(bool m,
                           const _T& on_true);
 
     // select_zero_or_val m ? _T(0) : on_false
-    template <class _M, class _T>
-    _T select_zero_or_val(const _M& m,
+    template <class _T>
+    _T select_zero_or_val(bool m,
                           const _T& on_false);
 
     // any_of
@@ -40,36 +40,36 @@ namespace cftal {
 
 }
 
-template <typename _M, typename _T>
+template <typename _T>
 inline
 _T
-cftal::select(const _M& m, const _T& on_true, const _T& on_false)
+cftal::select(bool m, const _T& on_true, const _T& on_false)
 {
     return m ? on_true : on_false;
 }
 
-template <typename _M, typename _T>
+template <typename _T>
 inline
 _T
-cftal::if_else(const _M& m, const _T& on_true, const _T& on_false)
+cftal::if_else(bool m, const _T& on_true, const _T& on_false)
 {
     return select(m, on_true, on_false);
 }
 
-template <typename _M, typename _T>
+template <typename _T>
 inline
 _T
 cftal::
-select_val_or_zero(const _M& m, const _T& on_true)
+select_val_or_zero(bool m, const _T& on_true)
 {
     return select(m, on_true, _T(0));
 }
 
-template <typename _M, typename _T>
+template <typename _T>
 inline
 _T
 cftal::
-select_zero_or_val(const _M& m, const _T& on_false)
+select_zero_or_val(bool m, const _T& on_false)
 {
     return select(m, _T(0), on_false);
 }
