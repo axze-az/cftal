a73fdd139bb162452356e51f931e1547daf4a2e4
axel <axel@18305beb-b1e2-0310-bcd8-b14b5419b1d7>
2021-11-12 17:09:09 +0000

    cftal::xxx_f32_to_bf16: addition of comments and introduction of rounding functions
    git-svn-id: file:///usr/local/svn/cpplib/trunk/cftal@4713 18305beb-b1e2-0310-bcd8-b14b5419b1d7

--- include/cftal/bf16.h
+++ include/cftal/bf16.h
@@ -19,105 +19,160 @@ namespace cftal {
     using mf_bf16_t = uint16_t;
 
     namespace impl {
-
+        
+        // round f32 to bf16 using round to nearest even rounding
+        template <std::size_t _N>
+        vec<float, _N>
+        _rne_f32_to_bf16(const vec<float, _N>& v);
+        
+        // convert  vec<f32, _N> to vec<mf_bf16_t, _N> using 
+        // round to nearest even rounding
         template <std::size_t _N>
         vec<mf_bf16_t, _N>
         _cvt_f32_to_bf16(const vec<float, _N>& v);
+        
+        // round f32 to bf16 using round to zero rounding
+        template <std::size_t _N>
+        vec<float, _N>
+        _rz_f32_to_bf16(const vec<float, _N>& v);        
 
+        // convert vec<f32, _N> to vec<mf_bf16_t, _N> using
+        // round to zero rounding
         template <std::size_t _N>
         vec<mf_bf16_t, _N>
         _cvt_f32_to_bf16_rz(const vec<float, _N>& v);
 
+        // convert vec<mf_bf16_t, _N> to vec<f32, _N> 
         template <std::size_t _N>
         vec<float, _N>
         _cvt_bf16_to_f32(const vec<mf_bf16_t, _N>& v);
 
     }
 
-    // template for conversion
+    // convert  vec<f32, _N> to vec<mf_bf16_t, _N> using 
+    // round to nearest even rounding
     template <std::size_t _N>
     vec<mf_bf16_t, _N>
     cvt_f32_to_bf16(const vec<float, _N>& v);
 
+    // convert  vec<f32, 1> to vec<mf_bf16_t, 1> using 
+    // round to nearest even rounding
     vec<mf_bf16_t, 1>
     cvt_f32_to_bf16(const vec<float, 1>& v);
 
+    // convert  vec<f32, 2> to vec<mf_bf16_t, 2> using 
+    // round to nearest even rounding
     vec<mf_bf16_t, 2>
     cvt_f32_to_bf16(const vec<float, 2>& v);
 
+    // convert  vec<f32, 4> to vec<mf_bf16_t, 4> using 
+    // round to nearest even rounding
     vec<mf_bf16_t, 4>
     cvt_f32_to_bf16(const vec<float, 4>& v);
 
+    // convert  vec<f32, 8> to vec<mf_bf16_t, 8> using 
+    // round to nearest even rounding
     vec<mf_bf16_t, 8>
     cvt_f32_to_bf16(const vec<float, 8>& v);
 
-    // template for conversion
+    // convert  vec<f32, 16> to vec<mf_bf16_t, 16> using 
+    // round to nearest even rounding
+    vec<mf_bf16_t, 16>
+    cvt_f32_to_bf16(const vec<float, 16>& v);
+
+    // convert vec<f32, _N> to vec<mf_bf16_t, _N> using
+    // round to zero rounding
     template <std::size_t _N>
     vec<mf_bf16_t, _N>
     cvt_f32_to_bf16_rz(const vec<float, _N>& v);
 
+    // convert vec<f32, 1> to vec<mf_bf16_t, 1> using
+    // round to zero rounding
     vec<mf_bf16_t, 1>
     cvt_f32_to_bf16_rz(const vec<float, 1>& v);
 
+    // convert vec<f32, 2> to vec<mf_bf16_t, 2> using
+    // round to zero rounding
     vec<mf_bf16_t, 2>
     cvt_f32_to_bf16_rz(const vec<float, 2>& v);
 
+    // convert vec<f32, 4> to vec<mf_bf16_t, 4> using
+    // round to zero rounding
     vec<mf_bf16_t, 4>
     cvt_f32_to_bf16_rz(const vec<float, 4>& v);
 
+    // convert vec<f32, 8> to vec<mf_bf16_t, 8> using
+    // round to zero rounding
     vec<mf_bf16_t, 8>
     cvt_f32_to_bf16_rz(const vec<float, 8>& v);
 
-    // template for conversion
+    // convert vec<f32, 8> to vec<mf_bf16_t, 8> using
+    // round to zero rounding
+    vec<mf_bf16_t, 16>
+    cvt_f32_to_bf16_rz(const vec<float, 16>& v);
+    
+    // convert vec<mf_bf16_t, _N> to vec<f32, _N> 
     template <std::size_t _N>
     vec<float, _N>
     cvt_bf16_to_f32(const vec<mf_bf16_t, _N>& v);
 
+    // convert vec<mf_bf16_t, 1> to vec<f32, 1> 
     vec<float, 1>
     cvt_bf16_to_f32(const vec<mf_bf16_t, 1>& v);
 
+    // convert vec<mf_bf16_t, 2> to vec<f32, 2> 
     vec<float, 2>
     cvt_bf16_to_f32(const vec<mf_bf16_t, 2>& v);
 
+    // convert vec<mf_bf16_t, 4> to vec<f32, 4> 
     vec<float, 4>
     cvt_bf16_to_f32(const vec<mf_bf16_t, 4>& v);
 
+    // convert vec<mf_bf16_t, 8> to vec<f32, 8> 
     vec<float, 8>
     cvt_bf16_to_f32(const vec<mf_bf16_t, 8>& v);
 
+    // convert vec<mf_bf16_t, 16> to vec<f32, 16> 
+    vec<float, 16>
+    cvt_bf16_to_f32(const vec<mf_bf16_t, 16>& v);
+}
+
+template <std::size_t _N>
+cftal::vec<float, _N>
+cftal::impl::_rne_f32_to_bf16(const vec<float, _N>& v)
+{
+    using vf_type = vec<float, _N>;
+    vf_type vne=round_to_nearest_even_last_bits<16>(v);
+    // infinities are not affected by rounding
+    typename vf_type::mask_type is_nan=isnan(v);
+    vne = select(is_nan, v, vne);
+    return vne;
 }
 
 template <std::size_t _N>
 cftal::vec<cftal::mf_bf16_t, _N>
 cftal::impl::_cvt_f32_to_bf16(const vec<float, _N>& v)
 {
-#if 1
-    using vf_type = vec<float, _N>;
-    vf_type vne=round_to_nearest_even_last_bits<16>(v);
-    typename vf_type::mask_type infnan=isinf(v)|isnan(v);
-    vne = select(infnan, v, vne);
+    auto vne = _rne_f32_to_bf16(v);
     auto t=as<vec<cftal::mf_bf16_t, 2*_N> >(vne);
-#else
-    using vi2_type= vec<int32_t, _N>;
-    using vmi2_type = typename vec<int32_t, _N>::mask_type;
-    vi2_type i= as<vi2_type>(v);
-    // normal case: round the mantissa only
-    const int32_t bias=0x7fff;
-    vi2_type mt_odd= (i>>16) & 1;
-    vi2_type in =  i + (mt_odd + bias);
-    // keep nans
-    auto is_nan= isnan(v);
-    in = select(as<vmi2_type>(is_nan), i, in);
-    auto t=as<vec<cftal::mf_bf16_t, 2*_N> >(in);
-#endif
     auto r=odd_elements(t);
     return r;
+}
+
+template <std::size_t _N>
+cftal::vec<float, _N>
+cftal::impl::_rz_f32_to_bf16(const vec<float, _N>& v)
+{
+    // zero the last 16 bits
+    auto vne=round_to_zero_last_bits<16>(v);
+    return vne;    
 }
 
 template <std::size_t _N>
 cftal::vec<cftal::mf_bf16_t, _N>
 cftal::impl::_cvt_f32_to_bf16_rz(const vec<float, _N>& v)
 {
+    // no masking of the trailing 16 bits is required
     auto t=as<vec<cftal::mf_bf16_t, 2*_N> >(v);
     auto r=odd_elements(t);
     return r;
@@ -169,6 +224,13 @@ cftal::cvt_f32_to_bf16(const vec<float, 8>& v)
     return impl::_cvt_f32_to_bf16(v);
 }
 
+inline
+cftal::vec<cftal::mf_bf16_t, 16>
+cftal::cvt_f32_to_bf16(const vec<float, 16>& v)
+{
+    return impl::_cvt_f32_to_bf16(v);
+}
+
 template <std::size_t _N>
 inline
 cftal::vec<cftal::mf_bf16_t, _N>
@@ -206,6 +268,13 @@ cftal::cvt_f32_to_bf16_rz(const vec<float, 8>& v)
     return impl::_cvt_f32_to_bf16_rz(v);
 }
 
+inline
+cftal::vec<cftal::mf_bf16_t, 16>
+cftal::cvt_f32_to_bf16_rz(const vec<float, 16>& v)
+{
+    return impl::_cvt_f32_to_bf16_rz(v);
+}
+
 template <std::size_t _N>
 inline
 cftal::vec<float, _N>
@@ -239,6 +308,13 @@ cftal::cvt_bf16_to_f32(const vec<mf_bf16_t, 4>& v)
 inline
 cftal::vec<float, 8>
 cftal::cvt_bf16_to_f32(const vec<mf_bf16_t, 8>& v)
+{
+    return impl::_cvt_bf16_to_f32(v);
+}
+
+inline
+cftal::vec<float, 16>
+cftal::cvt_bf16_to_f32(const vec<mf_bf16_t, 16>& v)
 {
     return impl::_cvt_bf16_to_f32(v);
 }
