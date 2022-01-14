6f40e04c39a106df596b2e49432a50a8e5dc22aa
axel <axel@18305beb-b1e2-0310-bcd8-b14b5419b1d7>
2022-01-14 17:20:15 +0000

    removal of trailing whitepsaces
    git-svn-id: file:///usr/local/svn/cpplib/trunk/cftal@4862 18305beb-b1e2-0310-bcd8-b14b5419b1d7

--- include/cftal/type_traits.h
+++ include/cftal/type_traits.h
@@ -22,48 +22,48 @@
 #include <type_traits>
 
 namespace cftal {
-    
+
     // is_integral similiar to std::is_integral but we want
     // to add specializations
     template <typename _T>
     struct is_integral : public std::is_integral<_T> {};
-    
+
     template< class T >
-    inline constexpr bool 
+    inline constexpr bool
     is_integral_v = is_integral<T>::value;
 
-    // is_floating_point similiar to std::is_floating_point but we 
+    // is_floating_point similiar to std::is_floating_point but we
     // want to add specializations
     template <typename _T>
     struct is_floating_point : public std::is_floating_point<_T> {};
 
     template< class T >
-    inline constexpr bool 
+    inline constexpr bool
     is_floating_point_v = is_floating_point<T>::value;
 
     // is_signed similiar to std::is_signed but we want
     // to add specializations
     template <typename _T>
     struct is_signed : public std::is_signed<_T> {};
-    
+
     template< class T >
-    inline constexpr bool 
+    inline constexpr bool
     is_signed_v = is_signed<T>::value;
 
     // is_unsigned similiar to std::is_unsigned but we want
     // to add specializations
     template <typename _T>
     struct is_unsigned : public std::is_unsigned<_T> {};
-    
+
     template< class T >
-    inline constexpr bool 
+    inline constexpr bool
     is_unsigned_v = is_unsigned<T>::value;
 
     // make_signed similiar to std::make_signed but we want
     // to add specializations
     template <typename _T>
     struct make_signed : public std::make_signed<_T> {};
-    
+
     template< class T >
     using make_signed_t = typename make_signed<T>::type;
 
@@ -73,7 +73,7 @@ namespace cftal {
     struct make_unsigned : public std::make_unsigned<_T> {};
 
     template< class T >
-    using make_unsigned_t = typename make_unsigned<T>::type;    
+    using make_unsigned_t = typename make_unsigned<T>::type;
 }
 
 #endif
