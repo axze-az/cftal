d174314c67b241e301804cb1f21e66a3f2327847
axel <axel@18305beb-b1e2-0310-bcd8-b14b5419b1d7>
2022-10-22 15:23:07 +0000

    improvements in perm1_v8u32
    git-svn-id: file:///usr/local/svn/cpplib/trunk/cftal@5550 18305beb-b1e2-0310-bcd8-b14b5419b1d7

--- test/test-v8u32.cc
+++ test/test-v8u32.cc
@@ -29,6 +29,10 @@ bool cftal::test::spec_v8u32()
 {
     bool r=true;
     using type=uint32_t;
+
+    r &= perm1_v8<type,  3,  2,  1,  0,  7,  6,  5,  4>();
+    r &= perm1_v8<type,  3, -1,  1,  0, -1,  6,  5,  4>();
+
     r &= perm1_v8<type, -1, -1, -1, -1, -1, -1, -1, -1>();
     r &= perm1_v8<type,  0,  1,  2,  3,  4,  5,  6,  7>();
     r &= perm1_v8<type,  0, 0, 0, 0, 0, 0, 0, 0>();
@@ -40,7 +44,7 @@ bool cftal::test::spec_v8u32()
     r &= perm1_v8<type,  1, 2, 3,-1, 5, 6, 7,-1>();
     r &= perm1_v8<type,  2, 3,-1,-1, 6, 7,-1,-1>();
     r &= perm1_v8<type,  3,-1,-1,-1, 7,-1,-1,-1>();
-    
+
     r &= perm1_v8<type,  0, 1, 0, 1, 4, 5, 4, 5>();
     r &= perm1_v8<type,  2, 3, 2, 3, 6, 7, 6, 7>();
     r &= perm1_v8<type,  0, -1, 1, -1, 2, -1, 3, -1>();
@@ -48,13 +52,13 @@ bool cftal::test::spec_v8u32()
 
     r &= perm2_v8<type,  0, 1, 8, 9, 4, 5, 12, 13>();
     r &= perm2_v8<type,  8, 9, 0, 1, 12, 13, 4, 5>();
-	
+
     r &= perm2_v8<type,  2, 3, 10, 11, 6, 7, 14, 15>();
     r &= perm2_v8<type, 10, 11, 2, 3, 14, 15, 6, 7>();
-    
+
     r &= perm2_v8<type,  0, 8, 1, 9, 4, 12, 5, 13>();
     r &= perm2_v8<type,  8, 0, 9, 1, 12, 4, 13, 5>();
-	
+
     r &= perm2_v8<type,  2, 10, 3, 11, 6, 14, 7, 15>();
     r &= perm2_v8<type, 10, 2, 11, 3, 14, 6, 15, 7>();
 
@@ -62,7 +66,7 @@ bool cftal::test::spec_v8u32()
     r &= perm2_v8<type, 0, 1, 2, 3, 12, 13, 14, 15>();
     r &= perm2_v8<type, 4, 5, 6, 7, 8, 9, 10, 11>();
     r &= perm2_v8<type, 4, 5, 6, 7, 12, 13, 14, 15 >();
-    
+
     return r;
 }
 
