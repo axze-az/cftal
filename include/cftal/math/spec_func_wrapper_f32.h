317388ec543c82985908dd5d7a772af7d21e1029
axel <axel@18305beb-b1e2-0310-bcd8-b14b5419b1d7>
2022-11-02 16:49:58 +0000

    lgamma: move the f64 implementation for f32 vectors into spec_func_loprec_core<double, _T>
    git-svn-id: file:///usr/local/svn/cpplib/trunk/cftal@5571 18305beb-b1e2-0310-bcd8-b14b5419b1d7

--- include/cftal/math/spec_func_wrapper_f32.h
+++ include/cftal/math/spec_func_wrapper_f32.h
@@ -28,6 +28,7 @@
 #include <cftal/math/impl_d_real_constants_f64.h>
 
 #define __CFTAL_CFG_USE_VF64_FOR_VF32_TGAMMA__ 1
+#define __CFTAL_CFG_USE_VF64_FOR_VF32_LGAMMA__ 1
 
 namespace cftal {
     namespace math {
@@ -38,8 +39,9 @@ namespace cftal {
             : public spec_func_core<float, _T> {
             using base_type = spec_func_core<float, _T>;
             using vf_type = typename base_type::vf_type;
-            using vmi_type = typename base_type::vmi_type;
             using vmf_type = typename base_type::vmf_type;
+            using vi_type = typename base_type::vi_type;
+            using vmi_type = typename base_type::vmi_type;
 
             using f64_traits = typename _T::vhf_traits;
             using f64_core = spec_func_loprec_core<double, f64_traits>;
@@ -52,6 +54,11 @@ namespace cftal {
             static
             vf_type
             tgamma_k(arg_t<vf_type> x, arg_t<vmf_type> x_lt_0);
+#endif
+#if __CFTAL_CFG_USE_VF64_FOR_VF32_LGAMMA__>0
+            static
+            vf_type
+            lgamma_k(arg_t<vf_type> xc, vi_type* signp);
 #endif
        };
     }
@@ -68,6 +75,20 @@ tgamma_k(arg_t<vf_type> x, arg_t<vmf_type> x_lt_0)
     vmi_type x_lt_0_i=_T::vmf_to_vmi(x_lt_0);
     vmhf_type x_lt_0_d= f64_traits::vmi_to_vmf(x_lt_0_i);
     vmhf_type rd=f64_core::tgamma_k(xd, x_lt_0_d);
+    vf_type r=cvt<vf_type>(rd);
+    return r;
+}
+#endif
+
+#if __CFTAL_CFG_USE_VF64_FOR_VF32_LGAMMA__>0
+template <typename _T>
+inline
+typename cftal::math::spec_func_wrapper<float, _T>::vf_type
+cftal::math::spec_func_wrapper<float, _T>::
+lgamma_k(arg_t<vf_type> x, vi_type* signp)
+{
+    vhf_type xd=cvt<vhf_type>(x);
+    vmhf_type rd=f64_core::lgamma_k(xd, signp);
     vf_type r=cvt<vf_type>(rd);
     return r;
 }
