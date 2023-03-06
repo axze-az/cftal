798d4bc1daf0827c7e544e8870f0a7a1d59090cf
axel <axel@18305beb-b1e2-0310-bcd8-b14b5419b1d7>
2023-03-06 16:33:35 +0000

    test of asinh: move the domains to test into check_asin.h
    git-svn-id: file:///usr/local/svn/cpplib/trunk/cftal@5749 18305beb-b1e2-0310-bcd8-b14b5419b1d7

--- test/test-asinh-f32.cc
+++ test/test-asinh-f32.cc
@@ -21,56 +21,11 @@
 int main(int argc, char** argv)
 {
     using namespace cftal::test;
-#if 1
-    const func_domain<float> di[]={
-        std::make_pair(-std::numeric_limits<float>::max(),
-                        std::numeric_limits<float>::max())
-    };
-    const int shifts[]={0};
-    int r=program<check_asinh<float>, 16, 1, 0x8000>(argc,
-                                                     argv,
-                                                     mpfr_asinh,
-                                                     di,
-                                                     shifts);
+    int r=program<check_asinh<float>, 16, 1, 0x8000>(
+        argc,
+        argv,
+        mpfr_asinh,
+        domain_asinh<float>::domains,
+        domain_asinh<float>::shifts);
     return r;
-#else
-    std::cout << std::setprecision(18) << std::scientific;
-    std::cerr << std::setprecision(18) << std::scientific;
-#if 0
-    cftal::v1f32 t=-2.834401250000000000e+06f;
-    std::pair<float, float> rp, rn;
-    float fp=call_mpfr::func(t(), mpfr_asinh, &rp);
-    float fn=call_mpfr::func(-t(), mpfr_asinh, &rn);
-
-    std::cout << fp << std::endl
-              << rp.first << std::endl
-              << rp.second << std::endl;
-    std::cout << fn << std::endl
-              << rn.first << std::endl
-              << rn.second << std::endl;
-#else
-    const int ulp=1;
-    const int _N=16;
-    bool rc=true;
-    bool speed_only=false;
-    std::size_t cnt=update_cnt(0x8000);
-    if ((argc > 1) && (std::string(argv[1]) == "--speed")) {
-        speed_only=true;
-        cnt *=8;
-    }
-    func_domain<float> d=std::make_pair(
-        -std::numeric_limits<float>::max(),
-        std::numeric_limits<float>::max());
-    auto us=std::make_shared<ulp_stats>();
-    exec_stats<_N> st;
-    rc &= of_fp_func_up_to<
-        float, _N, check_asinh<float> >::v(st, d, speed_only,
-                                           cmp_ulp<float>(ulp, us),
-                                           cnt);
-    std::cout << "ulps: "
-              << std::fixed << std::setprecision(4) << *us << std::endl;
-    std::cout << st << std::endl;
-    return (rc == true) ? 0 : 1;
-#endif
-#endif
 }
--- test/test-asinh-f64.cc
+++ test/test-asinh-f64.cc
@@ -23,49 +23,11 @@
 int main(int argc, char** argv)
 {
     using namespace cftal::test;
-#if 1
-    const func_domain<double> di[]={
-        std::make_pair(-std::numeric_limits<double>::max(),
-                        std::numeric_limits<double>::max())
-    };
-    const int shifts[]= {
-        0,
-    };
-    int r=program<check_asinh<double>, 8, 1, 0x8000>(argc,
-                                                     argv,
-                                                     mpfr_asinh,
-                                                     di,
-                                                     shifts);
+    int r=program<check_asinh<double>, 8, 1, 0x8000>(
+        argc,
+        argv,
+        mpfr_asinh,
+        domain_asinh<double>::domains,
+        domain_asinh<double>::shifts);
     return r;
-#else
-    std::cout << std::setprecision(18) << std::scientific;
-    std::cerr << std::setprecision(18) << std::scientific;
-    const int ulp=1;
-    const int _N=8;
-    bool rc=true;
-    bool speed_only=false;
-    std::size_t cnt=update_cnt(0x8000);
-    if ((argc > 1) && (std::string(argv[1]) == "--speed")) {
-        speed_only=true;
-        cnt *=8;
-    }
-#if 1
-    func_domain<double> d=std::make_pair(
-        -std::numeric_limits<double>::max(),
-        std::numeric_limits<double>::max());
-#else
-    func_domain<double> d=std::make_pair(M_SQRT2/2, 3.0);
-                                         // std::numeric_limits<double>::max());
-#endif
-    auto us=std::make_shared<ulp_stats>();
-    exec_stats<_N> st;
-    rc &= of_fp_func_up_to<
-        double, _N, check_asinh<double> >::v(st, d, speed_only,
-                                             cmp_ulp<double>(ulp, us),
-                                             cnt);
-    std::cout << "ulps: "
-              << std::fixed << std::setprecision(4) << *us << std::endl;
-    std::cout << st << std::endl;
-    return (rc == true) ? 0 : 1;
-#endif
 }
