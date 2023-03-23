9ec289d2bf265dd39712cd0d472d14eaf94a93e9
axel <axel@18305beb-b1e2-0310-bcd8-b14b5419b1d7>
2023-03-23 16:20:01 +0000

    test of exp10_mx2: move the domains to test into check_exp10_mx2.h
    git-svn-id: file:///usr/local/svn/cpplib/trunk/cftal@5767 18305beb-b1e2-0310-bcd8-b14b5419b1d7

--- test/test-exp10_mx2-f32.cc
+++ test/test-exp10_mx2-f32.cc
@@ -20,43 +20,12 @@
 
 int main(int argc, char** argv)
 {
-#if 1
     using namespace cftal::test;
-    const func_domain<float> di[]={
-        std::make_pair(0.0, 6.8)
-    };
-    const int shifts[]={
-        0
-    };
-    int r=program<check_exp10_mx2<float>, 16, 1, 0x8000>(argc,
-                                                         argv,
-                                                         mpfr_ext::exp10_mx2,
-                                                         di,
-                                                         shifts);
+    int r=program<check_exp10_mx2<float>, 16, 1, 0x8000>(
+        argc,
+        argv,
+        mpfr_ext::exp10_mx2,
+        domain_exp10_mx2<float>::domains,
+        domain_exp10_mx2<float>::shifts);
     return r;
-#else
-    using namespace cftal::test;
-    std::cout << std::setprecision(18) << std::scientific;
-    std::cerr << std::setprecision(18) << std::scientific;
-    const int ulp=1;
-    const int _N=16;
-    bool rc=true;
-    bool speed_only=false;
-    std::size_t cnt=update_cnt(0x8000);
-    if ((argc > 1) && (std::string(argv[1]) == "--speed")) {
-        speed_only=true;
-        cnt *=8;
-    }
-    func_domain<float> d=std::make_pair(0.0, 6.8);
-    exec_stats<_N> st;
-    auto us=std::make_shared<ulp_stats>();
-    rc &= of_fp_func_up_to<
-        float, _N, check_exp10_mx2<float> >::v(st, d, speed_only,
-                                                 cmp_ulp<float>(ulp, us),
-                                                 cnt);
-    std::cout << "ulps: "
-              << std::fixed << std::setprecision(4) << *us << std::endl;
-    std::cout << st << std::endl;
-    return (rc == true) ? 0 : 1;
-#endif
 }
