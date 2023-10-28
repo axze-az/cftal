15eeb183ed6c573ad83505dec358757b20725c3f
axel <az0123456@gmx.de>
2023-10-28 16:30:02 +0200

    add d_real_traits<vec<f16_t, _N>>

--- include/cftal/vec_d_real_traits.h
+++ include/cftal/vec_d_real_traits.h
@@ -21,8 +21,9 @@
 #include <cftal/config.h>
 #include <cftal/vec_t_n.h>
 #include <cftal/d_real.h>
-#include <cftal/d_real_traits_f32.h>
 #include <cftal/d_real_traits_f64.h>
+#include <cftal/d_real_traits_f32.h>
+#include <cftal/d_real_traits_f16.h>
 
 namespace cftal {
 
@@ -124,6 +125,56 @@ namespace cftal {
         scale_div_threshold() {
             // -126 + 24
             return 0x1.p-102f;
+        }
+
+    };
+
+    template <std::size_t _N>
+    struct d_real_traits<vec<f16_t, _N> > : public has_fma<f16_t> {
+        using cmp_result_type = typename vec<f16_t, _N>::mask_type;
+        using int_type = vec<int16_t, _N>;
+
+        static
+        bool any_of_v(const cmp_result_type& b) {
+            return any_of(b);
+        }
+
+        static
+        bool all_of_v(const cmp_result_type& b) {
+            return all_of(b);
+        }
+
+        static
+        bool none_of_v(const cmp_result_type& b) {
+            return none_of(b);
+        }
+
+        static
+        vec<f16_t, _N>
+        sel(const cmp_result_type& s,
+            const vec<f16_t, _N>& on_true,
+            const vec<f16_t, _N>& on_false) {
+            return select(s, on_true, on_false);
+        }
+
+        static
+        void
+        split(const vec<f16_t, _N> & a,
+              vec<f16_t, _N>& h,
+              vec<f16_t, _N>& l) {
+            const f16_t msk=as<f16_t>(uint16_t(0xffc0));
+            const vec<f16_t, _N> th = a & msk;
+            const vec<f16_t, _N> tl = a - th;
+            h = th;
+            l = tl;
+        }
+
+        // constexpr
+        static
+        f16_t
+        scale_div_threshold() {
+            // -15 + 11
+            return 0x1.p-3_f16;
         }
 
     };
