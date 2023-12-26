bdb7109af9f27362ecbb7b8ff9b9a5276217441f
axel <az0123456@gmx.de>
2023-12-26 15:11:13 +0100

    f16: add tests for trigpi functions
    add tests for sinpi, cospi, tanpi and sinpicospi function
    for vectors of binary16

---
+++ test/test-sinpicospi-f16.cc
@@ -0,0 +1,55 @@
+//
+// Copyright (C) 2010-2023 Axel Zeuner
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+//
+#include "cftal/test/program.h"
+#include "cftal/test/check_sinpicospi.h"
+#include "cftal/vec_f16.h"
+#include <iostream>
+#include <iomanip>
+
+int main(int argc, char** argv)
+{
+    using namespace cftal::test;
+    using cftal::f16_t;
+    using cftal::operator""_f16;
+    cpu_times_to_stdout tt;
+    pgm_args ags=parse(argc, argv, 0x4000);
+
+    std::cout << std::setprecision(18) << std::scientific;
+    std::cerr << std::setprecision(18) << std::scientific;
+    const int ulp=1;
+    const int _N=32;
+
+    bool rc=true;
+    auto dp=std::make_pair(-0x1.5p12_f16, 0x1.5p12_f16);
+    auto us_sin=std::make_shared<ulp_stats>();
+    auto us_cos=std::make_shared<ulp_stats>();
+    exec_stats<_N> st;
+    struct cmp_ulp<std::pair<f16_t, f16_t> > cmp(ulp, us_sin, us_cos);
+    rc &= of_fp_func_up_to<
+        f16_t, _N, check_sinpicospi<f16_t> >::v(st, dp,
+                                                ags._speed_only,
+                                                ags._mt,
+                                                cmp,
+                                                ags._cnt);
+    std::cout << "sinpi ulps: "
+              << std::fixed << std::setprecision(4) << *us_sin << std::endl;
+    std::cout << "cospi ulps: "
+              << std::fixed << std::setprecision(4) << *us_cos << std::endl;
+    std::cout << st << std::endl;
+    return (rc == true) ? 0 : 1;
+}
