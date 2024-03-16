49d755359ae9f01c150f88fb50f7250bde83db84
axel <az0123456@gmx.de>
2024-03-16 17:09:45 +0100

    add missing file

---
+++ include/cftal/math/spec_func_wrapper_f16.h
@@ -0,0 +1,37 @@
+//
+// Copyright (C) 2010-2024 Axel Zeuner
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+//
+#if !defined (__CFTAL_MATH_SPEC_FUNC_WRAPPER_F16_H__)
+#define __CFTAL_MATH_SPEC_FUNC_WRAPPER_F16_H__ 1
+
+#include <cftal/config.h>
+#include <cftal/math/spec_func_core_f16.h>
+
+#if 0
+namespace cftal {
+    namespace math {
+        // specialization of elem_func_wrapper for f16_t and different
+        // traits
+        template <typename _T>
+        struct spec_func_wrapper<f16_t, _T>
+            : public spec_func_core<f16_t, _T> {
+       };
+    }
+}
+#endif
+
+#endif // __CFTAL_MATH_SPEC_FUNC_WRAPPER_F16_H__
