09de247877c93624d61921c7dc911ffd5d3c882a
axdev <az0123456@gmx.de>
2024-04-27 15:14:09 +0200

    correction of an include guard

--- include/cftal/cvt.h
+++ include/cftal/cvt.h
@@ -16,7 +16,7 @@
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
 //
 #if !defined (__CFTAL_CVT_H__)
-#define __CFTAL_VEC_CVT_H__ 1
+#define __CFTAL_CVT_H__ 1
 
 #include <cftal/config.h>
 
