7a2976dd5d90317b28406f14dbef0981b86318d4
axel <az0123456@gmx.de>
2024-05-25 11:32:15 +0200

    improve format

--- include/cftal/math/elem_func_core_f16.h
+++ include/cftal/math/elem_func_core_f16.h
@@ -2524,8 +2524,8 @@ pow_k(arg_t<vf_type> x, arg_t<vf_type> y)
 {
     vf_type abs_x= abs(x);
     vdf_type lnx= __log_tbl_k12(abs_x);
-    vdf_type ylnx;
 
+    vdf_type ylnx;
     d_ops::mul122(ylnx[0], ylnx[1], y, lnx[0], lnx[1]);
 
     vf_type xrh, xrl;
