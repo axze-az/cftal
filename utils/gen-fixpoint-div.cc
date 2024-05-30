5d8750c459f9f633773d5f5c2c5abae19de3e79a
axdev <az0123456@gmx.de>
2024-05-30 16:35:27 +0200

    adapt test program for fix point division
    adapt the test program for fix point division also to 16bit integers

--- utils/gen-fixpoint-div.cc
+++ utils/gen-fixpoint-div.cc
@@ -23,18 +23,27 @@ int main(int argc, char** argv)
 {
     using namespace cftal;
 
-    // 1/3 = x/2^16
-    // x=2^16/3;
+    // 1/d = x/2^s
+    // x=2^s/d;
+    // d --> divisor
+    // s --> shift_1_d
 
-    const int32_t divisor=9;
-    const int32_t shift_1_d=14;
+    const int32_t divisor=12;
+    const int32_t shift_1_d=0x11;
     const int32_t fac_1_d=((1<<shift_1_d)+(divisor-1))/divisor;
-#if 0
+#define FP_BITS 32
+#if FP_BITS==64
     const int32_t bias=1023;
     const int32_t bits=53;
-#else
+    using int_type=int32_t;
+#elif FP_BITS==32
     const int32_t bias=127;
     const int32_t bits=24;
+    using int_type=int32_t;
+#elif FP_BITS==16
+    const int32_t bias=15;
+    const int32_t bits=11;
+    using int_type=int16_t;
 #endif
 
     std::cout << "const int32_t shift_1_" << divisor
@@ -42,27 +51,27 @@ int main(int argc, char** argv)
     std::cout << "const int32_t fac_1_" << divisor
               << "= 0x" << std::hex << fac_1_d << std::dec<<";\n";
 
-    for (int32_t i=-bias-bits; i<bias+1; ++i) {
+    for (int_type i=-bias-bits; i<bias+1; ++i) {
         // round down division
-        int32_t q=i/divisor;
+        int_type q=i/divisor;
         if (i<0 && (i%divisor))
             --q;
         // int32_t p=((i*fac_1_3)>>shift_1_3) - (i>>31);
-        int32_t p = (((i+divisor*bias)*fac_1_d)>>shift_1_d) - bias;
-        int32_t r=i-divisor*p;
+        int_type p = (((i+divisor*bias)*fac_1_d)>>shift_1_d) - bias;
+        int_type r=i-divisor*p;
         if (p != q) {
             std::cout << "i=" << i << " q=" << q << " qp="
                       << p << " r=" << r << std::endl;
         }
     }
-    for (int32_t i=-(bias+bits+bias+2); i<bias+bits+bias+2; ++i) {
+    for (int_type i=-(bias+bits+bias+2); i<bias+bits+bias+2; ++i) {
         // round down division
-        int32_t q=i/divisor;
+        int_type q=i/divisor;
         if (i<0 && (i%divisor))
             --q;
         // int32_t p=((i*fac_1_3)>>shift_1_3) - (i>>31);
-        int32_t p = (((i+divisor*bias)*fac_1_d)>>shift_1_d) - bias;
-        int32_t r=i-divisor*p;
+        int_type p = (((i+divisor*bias)*fac_1_d)>>shift_1_d) - bias;
+        int_type r=i-divisor*p;
         if (p != q) {
             std::cout << "i=" << i << " q=" << q << " qp="
                       << p << " r=" << r << std::endl;
