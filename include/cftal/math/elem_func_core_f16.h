4e80a02bdb86045bb47aed085b12a214ab63c19b
axdev <az0123456@gmx.de>
2024-06-22 07:57:40 +0200

    native f16: fix for trigpi argument reduction

--- include/cftal/math/elem_func_core_f16.h
+++ include/cftal/math/elem_func_core_f16.h
@@ -3223,11 +3223,13 @@ __reduce_trigpi_arg(vf_type& xrh, vf_type& xrl, arg_t<vf_type> xc)
 {
     vf_type xt2=xc+xc;
     vf_type fh= rint(xt2);
-    xrh = xc - 0.5f * fh;
+    xrh = xc - 0.5_f16 * fh;
+    // poor mans fmod:
+    fh = __fmod<4>(fh);
     // no need for fmod<4>(fh) here because |int(fh)| < |max integer|
     using ctbl=impl::d_real_constants<d_real<f16_t>, f16_t>;
     d_ops::mul122(xrh, xrl, xrh, ctbl::m_pi[0], ctbl::m_pi[1]);
-    vi_type q= _T::cvt_f_to_i(xt2);
+    vi_type q= _T::cvt_f_to_i(fh);
     return q;
 }
 
