d806da4c3f07297e0a7fee70ebd6102b080dcff9
axel <az0123456@gmx.de>
2024-08-01 17:53:58 +0200

    correction of the last commit

--- include/cftal/bf16.h
+++ include/cftal/bf16.h
@@ -182,7 +182,7 @@ cftal::impl::_rz_f32_to_bf16(const vec<float, _N>& v)
     // zero the last 16 bits
     auto vne=round_to_zero_last_bits<16>(v);
     // flush subnormals to zero
-    typename vf_type::mask_type is_subnorm=
+    typename vec<float, _N>::mask_type is_subnorm=
         abs(v) < std::numeric_limits<float>::min();
     constexpr const float mz=-0.0f;
     vne = select(is_subnorm, (v & mz), vne);
