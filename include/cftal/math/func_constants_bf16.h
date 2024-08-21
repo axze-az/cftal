e97716a94a6e6ac62b5bb5693c2c39a1260770ad
axdev <az0123456@gmx.de>
2024-08-21 17:45:14 +0200

    bf16_t: insert the first real bf16 constants
    use real bf16 constants for exp, expm1 and exp2 in cftal::math::func_constants<bf16_t>
    and return bf16_t from all functions

--- include/cftal/math/func_constants_bf16.h
+++ include/cftal/math/func_constants_bf16.h
@@ -29,99 +29,99 @@ namespace cftal {
 
         template <>
         struct func_constants<bf16_t> {
-            static constexpr float
-            min_normal() { return 0x1p-126f; }
+            static constexpr bf16_t
+            min_normal() { return 0x1p-126_bf16; }
 
-            static constexpr float
-            max_denormal() { return 1.175494210692441075487029e-38f; }
+            static constexpr bf16_t
+            max_denormal() { return 1.175494210692441075487029e-38_bf16; }
 
             // exp(x) == +inf for x >=
-            static constexpr float
-            exp_hi_inf() { return 89.0f /* 8.872283935546875000000000e+01f*/ ; }
+            static constexpr bf16_t
+            exp_hi_inf() { return 8.900000000000000000000000e+01_bf16; }
             // exp(x) == 0 for x <=
-            static constexpr float
-            exp_lo_zero() { return -1.039720840454101562500000e+02f; }
+            static constexpr bf16_t
+            exp_lo_zero() { return -8.750000000000000000000000e+01_bf16; }
 
             // expm1(x) == +inf for x >= 0
-            static constexpr float
-            expm1_hi_inf() { return 8.872283935546875000000000e+01f; }
+            static constexpr bf16_t
+            expm1_hi_inf() { return 8.900000000000000000000000e+01_bf16; }
             // expm1(x) == -1 for x <=
-            static constexpr float
-            expm1_lo_minus_one() { return -1.732868003845214843750000e+01f; }
+            static constexpr bf16_t
+            expm1_lo_minus_one() { return -6.250000000000000000000000e+00_bf16; }
 
             // exp2(x) == +inf for x >=
-            static constexpr float
-            exp2_hi_inf() { return 1.280000000000000000000000e+02f; }
+            static constexpr bf16_t
+            exp2_hi_inf() { return 1.280000000000000000000000e+02_bf16; }
             // exp2(x) == 0 for x <=
-            static constexpr float
-            exp2_lo_zero() { return -1.500000000000000000000000e+02f; }
+            static constexpr bf16_t
+            exp2_lo_zero() { return -1.265000000000000000000000e+02_bf16; }
 
             // exp2m1(x) == +inf for x >= 0
-            static constexpr float
-            exp2m1_hi_inf() { return 1.280000000000000000000000e+02f; }
+            static constexpr bf16_t
+            exp2m1_hi_inf() { return 1.280000000000000000000000e+02_bf16; }
             // exp2m1(x) == -1 for x <=
-            static constexpr float
-            exp2m1_lo_minus_one() { return -2.500000000000000000000000e+01f; }
+            static constexpr bf16_t
+            exp2m1_lo_minus_one() { return -2.500000000000000000000000e+01_bf16; }
 
             // exp10(x) == +inf for x >=
-            static constexpr float
-            exp10_hi_inf() { return 3.853184127807617187500000e+01f; }
+            static constexpr bf16_t
+            exp10_hi_inf() { return 3.853184127807617187500000e+01_bf16; }
             // exp10(x) == 0 for x <=
-            static constexpr float
-            exp10_lo_zero() { return -4.515450286865234375000000e+01f; }
+            static constexpr bf16_t
+            exp10_lo_zero() { return -4.515450286865234375000000e+01_bf16; }
 
             // exp10m1(x) == +inf for x >= 0
-            static constexpr float
-            exp10m1_hi_inf() { return 3.853184127807617187500000e+01f; }
+            static constexpr bf16_t
+            exp10m1_hi_inf() { return 3.853184127807617187500000e+01_bf16; }
             // exp10m1(x) == -1 for x <=
-            static constexpr float
-            exp10m1_lo_minus_one() { return -7.525750160217285156250000e+00f; }
+            static constexpr bf16_t
+            exp10m1_lo_minus_one() { return -7.525750160217285156250000e+00_bf16; }
 
             // cosh(x) == +inf for abs(x) >=
-            static constexpr float
-            cosh_hi_inf() { return 8.941599273681640625000000e+01f; }
+            static constexpr bf16_t
+            cosh_hi_inf() { return 8.941599273681640625000000e+01_bf16; }
 
             // sinh(x) == +inf for x >=
-            static constexpr float
-            sinh_hi_inf() { return 8.941599273681640625000000e+01f; }
+            static constexpr bf16_t
+            sinh_hi_inf() { return 8.941599273681640625000000e+01_bf16; }
             // sinh(x) == -inf for x <=
-            static constexpr float
-            sinh_lo_inf() { return -8.941599273681640625000000e+01f; }
+            static constexpr bf16_t
+            sinh_lo_inf() { return -8.941599273681640625000000e+01_bf16; }
 
             // tanh(x) == 1 for abs(x) >=
-            static constexpr float
-            tanh_one() { return 9.010913848876953125000000e+00f; }
+            static constexpr bf16_t
+            tanh_one() { return 9.010913848876953125000000e+00_bf16; }
 
-            static constexpr float
-            log_lo_fin() { return 1.401298464324817070923730e-45f; }
-            static constexpr float
-            log_lo_val() { return -1.032789306640625000000000e+02f; }
+            static constexpr bf16_t
+            log_lo_fin() { return 1.401298464324817070923730e-45_bf16; }
+            static constexpr bf16_t
+            log_lo_val() { return -1.032789306640625000000000e+02_bf16; }
 
-            static constexpr float
-            erf_lt_one_fin() { return 3.919205665588378906250000e+00f; }
+            static constexpr bf16_t
+            erf_lt_one_fin() { return 3.919205665588378906250000e+00_bf16; }
 
-            static constexpr float
-            erfc_gt_zero_fin() { return 1.005419445037841796875000e+01f; }
+            static constexpr bf16_t
+            erfc_gt_zero_fin() { return 1.005419445037841796875000e+01_bf16; }
 
-            static constexpr float
-            tgamma_hi_inf() { return 3.504010009765625000000000e+01f; }
+            static constexpr bf16_t
+            tgamma_hi_inf() { return 3.504010009765625000000000e+01_bf16; }
 
             // tgamma(x) == +- 0 for x<
-            static constexpr float
-            tgamma_lo_zero() { return -42.0f; }
+            static constexpr bf16_t
+            tgamma_lo_zero() { return -42.0_bf16; }
 
-            static constexpr float
-            lgamma_hi_inf() { return 4.085003425410169132208425e+36f; }
+            static constexpr bf16_t
+            lgamma_hi_inf() { return 4.085003425410169132208425e+36_bf16; }
 
-            static constexpr float
-            atan_equal_pi_2() { return 6.291977600000000000000000e+07f; }
+            static constexpr bf16_t
+            atan_equal_pi_2() { return 6.291977600000000000000000e+07_bf16; }
 
-            static constexpr float
+            static constexpr bf16_t
             sig_hi_one() {
                 // log(0x1p26)
-                return 18.0218257904052734375f;
+                return 18.0218257904052734375_bf16;
             }
-            static constexpr float
+            static constexpr bf16_t
             sig_le_eq_exp() {
                 return -sig_hi_one();
             }
