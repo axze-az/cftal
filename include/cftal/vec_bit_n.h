fdbb5e32c9d52fd378eb21dd129c5841e594dd93
axdev <az0123456@gmx.de>
2024-10-05 11:36:57 +0200

    vec<bit, _N> use call by constant reference utype
    vec<bit, _N> use call by constant reference utype argument instead of
    call by value, return a const reference from operator() for vec<bit, 1>

--- include/cftal/vec_bit_n.h
+++ include/cftal/vec_bit_n.h
@@ -87,7 +87,7 @@ namespace cftal {
         constexpr vec(const vec<bit, _N/2>& l, const vec<bit, _N/2>& h)
             : _v((utype(l())) |
                  ((utype(h()) << (_N/2)))) {}
-        constexpr vec(utype v) : _v( v & mask) {}
+        constexpr vec(const utype& v) : _v( v & mask) {}
         const utype& operator()() const { return _v; }
     private:
         utype _v;
@@ -120,8 +120,8 @@ namespace cftal {
         vec& operator=(const vec&) = default;
         vec& operator=(vec&&) = default;
         constexpr vec(const bit& v) : _v( v()!=0 ? 1 : 0) {}
-        constexpr vec(utype v) : _v( v & mask) {}
-        utype operator()() const { return _v; }
+        constexpr vec(const utype& v) : _v( v & mask) {}
+        const utype& operator()() const { return _v; }
     private:
         utype _v;
     };
