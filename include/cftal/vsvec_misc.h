4fc08036f2203a9aab3bf3a97504bde58af55cb6
axdev <az0123456@gmx.de>
2025-02-05 17:17:12 +0100

    add 2 dot_product overloads
    add dot_product(vsvec, stride, offset, vsvec) and
    dot_product(vsvec, vsvec, stride, offset, vsec) functions

--- include/cftal/vsvec_misc.h
+++ include/cftal/vsvec_misc.h
@@ -30,8 +30,12 @@ namespace cftal {
     template <typename _T, typename _A>
     _T
     dot_product(const vsvec<_T, _A>& a, int32_t stride_a, size_t offset_a,
-		const vsvec<_T, _A>& b);
-    
+                const vsvec<_T, _A>& b);
+
+    template <typename _T, typename _A>
+    _T
+    dot_product(const vsvec<_T, _A>& b,
+                const vsvec<_T, _A>& a, int32_t stride_a, size_t offset_a);
 }
 
 template <typename _T, typename _A>
@@ -45,34 +49,94 @@ dot_product(const vsvec<_T, _A>& a, const vsvec<_T, _A>& b)
     const size_t n1= s & ~(_N-1);
     _T r(0);
     if (n1) {
+        using v_t = vec<_T, _N>;
         const size_t n4= s & ~(_N4-1);
-        vec<_T, _N> a0(_T(0));
+        v_t r0(_T(0));
         if (n4) {
-            vec<_T, _N> a1(_T(0)), a2(_T(0)), a3(_T(0));
+            v_t r1(_T(0)), r2(_T(0)), r3(_T(0));
             for (size_t i=0; i<n4; i+=4*_N) {
-                a0 += a.template loadv<_N>(i+0*_N) *
-                    b.template loadv<_N>(i+0*_N);
-                a1 += a.template loadv<_N>(i+1*_N) *
-                    b.template loadv<_N>(i+1*_N);
-                a2 += a.template loadv<_N>(i+2*_N) *
-                    b.template loadv<_N>(i+2*_N);
-                a3 += a.template loadv<_N>(i+3*_N) *
-                    b.template loadv<_N>(i+3*_N);
+                v_t a0 = a.template loadv<_N>(i+0*_N);
+                v_t a1 = a.template loadv<_N>(i+1*_N);
+                v_t a2 = a.template loadv<_N>(i+2*_N);
+                v_t a3 = a.template loadv<_N>(i+3*_N);
+
+                r0 += a0 * b.template loadv<_N>(i+0*_N);
+                r1 += a1 * b.template loadv<_N>(i+1*_N);
+                r2 += a2 * b.template loadv<_N>(i+2*_N);
+                r3 += a3 * b.template loadv<_N>(i+3*_N);
             }
-            a2 += a3;
-            a0 += a1;
-            a0 += a2;
+            r2 += r3;
+            r0 += r1;
+            r0 += r2;
         }
         for (size_t i=n4; i<n1; i+= _N) {
-            a0 +=a.template loadv<_N>(i+0*_N)
+            r0 +=a.template loadv<_N>(i+0*_N)
                 * b.template loadv<_N>(i+0*_N);
         }
-        r = hadd(a0);
+        r = hadd(r0);
     }
     for (size_t i=n1; i<s; ++i) {
         r += a[i] * b[i];
     }
     return r;
+}
+
+template <typename _T, typename _A>
+cftal::vsvec<_T, _A>
+cftal::
+dot_product(const vsvec<_T, _A>& a, int32_t stride_a, size_t offset_a,
+            const vsvec<_T, _A>& b)
+{
+    constexpr const size_t _N=max_vec_size_specialized<_T>();
+    constexpr const size_t _N4=4*_N;
+
+    const size_t s= a.size();
+    const size_t n1= s & ~(_N-1);
+    _T r(0);
+    if (n1) {
+        using v_t = vec<_T, _N>;
+        using vi_t = vec<int32_t, _N>;
+        const size_t n4= s & ~(_N4-1);
+        vec<_T, _N> r0(_T(0));
+        vi_t idx_a=
+            impl::indices_from_stride<vi_t, int32_t>::create(stride_a);
+        auto lck_a=make_variable_lookup_table<_T>(idx_a);
+        if (n4) {
+            vec<_T, _N> r1(_T(0)), r2(_T(0)), r3(_T(0));
+            for (size_t i=0; i<n4; i+=4*_N) {
+                v_t a0 = lck_a.fromp(&a[(i+0*_N)*stride_a+offset_a]);
+                v_t a1 = lck_a.fromp(&a[(i+1*_N)*stride_a+offset_a]);
+                v_t a2 = lck_a.fromp(&a[(i+1*_N)*stride_a+offset_a]);
+                v_t a3 = lck_a.fromp(&a[(i+1*_N)*stride_a+offset_a]);
+
+                r0 += a0 * b.template loadv<_N>(i+0*_N);
+                r1 += a1 * b.template loadv<_N>(i+1*_N);
+                r2 += a2 * b.template loadv<_N>(i+2*_N);
+                r3 += a3 * b.template loadv<_N>(i+3*_N);
+            }
+            r2 += r3;
+            r0 += r1;
+            r0 += r2;
+        }
+        for (size_t i=n4; i<n1; i+= _N) {
+            v_t a0 = lck_a.fromp(&a[(i+0*_N)*stride_a+offset_a]);
+            r0 += a0 * b.template loadv<_N>(i+0*_N);
+        }
+        r = hadd(r0);
+    }
+    for (size_t i=n1; i<s; ++i) {
+        r += a[i*stride_a+offset_a] * b[i];
+    }
+    return r;
+}
+
+template <typename _T, typename _A>
+cftal::vsvec<_T, _A>
+cftal::
+dot_product(const vsvec<_T, _A>& b,
+            const vsvec<_T, _A>& a, int32_t stride_a, size_t offset_a)
+{
+    return dot_product(a, stride_a, offset_a, b);
 }
 
 // Local variables:
