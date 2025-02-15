addfaa325ab4aea043ff5e8fe8a7362e83b07ad1
axdev <az0123456@gmx.de>
2025-02-15 11:17:46 +0100

    vsvec: use the template dot product functions

--- include/cftal/vsvec_misc.h
+++ include/cftal/vsvec_misc.h
@@ -19,7 +19,8 @@
 #define __CFTAL_VSVEC_MISC_H__ 1
 
 #include <cftal/config.h>
-#include <cftal/vsvec_load_strided.h>
+#include <cftal/vec_load_strided.h>
+#include <cftal/impl/dot_product.h>
 #include <stdexcept>
 #include <sstream>
 
@@ -54,42 +55,7 @@ _T
 cftal::
 dot_product(const vsvec<_T, _A>& a, const vsvec<_T, _A>& b)
 {
-    constexpr const size_t _N=max_vec_size_specialized<_T>();
-    constexpr const size_t _N4=4*_N;
-    const size_t s= a.size();
-    const size_t n1= s & ~(_N-1);
-    _T r(0);
-    if (n1) {
-        using v_t = vec<_T, _N>;
-        const size_t n4= s & ~(_N4-1);
-        v_t r0(_T(0));
-        if (n4) {
-            v_t r1(_T(0)), r2(_T(0)), r3(_T(0));
-            for (size_t i=0; i<n4; i+=4*_N) {
-                v_t a0 = a.template loadv<_N>(i+0*_N);
-                v_t a1 = a.template loadv<_N>(i+1*_N);
-                v_t a2 = a.template loadv<_N>(i+2*_N);
-                v_t a3 = a.template loadv<_N>(i+3*_N);
-
-                r0 += a0 * b.template loadv<_N>(i+0*_N);
-                r1 += a1 * b.template loadv<_N>(i+1*_N);
-                r2 += a2 * b.template loadv<_N>(i+2*_N);
-                r3 += a3 * b.template loadv<_N>(i+3*_N);
-            }
-            r2 += r3;
-            r0 += r1;
-            r0 += r2;
-        }
-        for (size_t i=n4; i<n1; i+= _N) {
-            r0 +=a.template loadv<_N>(i+0*_N)
-                * b.template loadv<_N>(i+0*_N);
-        }
-        r = hadd(r0);
-    }
-    for (size_t i=n1; i<s; ++i) {
-        r += a[i] * b[i];
-    }
-    return r;
+    return impl::dot_product<_T, _T>(a.size(), a.cbegin(), b.cbegin());
 }
 
 template <typename _T, typename _A>
@@ -98,57 +64,9 @@ cftal::
 dot_product(const vsvec<_T, _A>& a, int32_t stride_a, size_t offset_a,
             const vsvec<_T, _A>& b)
 {
-    constexpr const size_t _N=max_vec_size_specialized<_T>();
-    constexpr const size_t _N4=4*_N;
-
-    if (__unlikely(stride_a== 0)) {
-        std::ostringstream s;
-        s << "stride_a==0 in "
-             "cftal::vsvec<_T, _A> cftal::"
-             "dot_product(const vsvec<_T, _A>& a, "
-             "int32_t stride_a, size_t offset_a, "
-             "const vsvec<_T, _A>& b)";
-        throw std::domain_error(s.str());
-    }
-
-    const size_t s= b.size();
-    const size_t n1= s & ~(_N-1);
-    _T r(0);
-    if (n1) {
-        using v_t = vec<_T, _N>;
-        using vi_t = vec<int32_t, _N>;
-        const size_t n4= s & ~(_N4-1);
-        vec<_T, _N> r0(_T(0));
-        vi_t idx_a=
-            impl::indices_from_stride<vi_t, int32_t>::create(stride_a);
-        auto lck_a=make_variable_lookup_table<_T>(idx_a);
-        if (n4) {
-            vec<_T, _N> r1(_T(0)), r2(_T(0)), r3(_T(0));
-            for (size_t i=0; i<n4; i+=4*_N) {
-                v_t a0 = lck_a.fromp(&a[(i+0*_N)*stride_a+offset_a]);
-                v_t a1 = lck_a.fromp(&a[(i+1*_N)*stride_a+offset_a]);
-                v_t a2 = lck_a.fromp(&a[(i+2*_N)*stride_a+offset_a]);
-                v_t a3 = lck_a.fromp(&a[(i+3*_N)*stride_a+offset_a]);
-
-                r0 += a0 * b.template loadv<_N>(i+0*_N);
-                r1 += a1 * b.template loadv<_N>(i+1*_N);
-                r2 += a2 * b.template loadv<_N>(i+2*_N);
-                r3 += a3 * b.template loadv<_N>(i+3*_N);
-            }
-            r2 += r3;
-            r0 += r1;
-            r0 += r2;
-        }
-        for (size_t i=n4; i<n1; i+= _N) {
-            v_t a0 = lck_a.fromp(&a[(i+0*_N)*stride_a+offset_a]);
-            r0 += a0 * b.template loadv<_N>(i+0*_N);
-        }
-        r = hadd(r0);
-    }
-    for (size_t i=n1; i<s; ++i) {
-        r += a[i*stride_a+offset_a] * b[i];
-    }
-    return r;
+    return impl::dot_product<_T, _T>(b.size(),
+                                     a.cbegin(), stride_a, offset_a,
+                                     b.cbegin());
 }
 
 template <typename _T, typename _A>
@@ -166,9 +84,6 @@ cftal::
 dot_product(const vsvec<_T, _A>& a, int32_t stride_a, size_t offset_a,
             const vsvec<_T, _A>& b, int32_t stride_b, size_t offset_b)
 {
-    constexpr const size_t _N=max_vec_size_specialized<_T>();
-    constexpr const size_t _N4=4*_N;
-
     if (__unlikely(stride_a == 0 || stride_b == 0)) {
         std::ostringstream s;
         s << "stride_" << ((stride_a == 0) ? 'a' : 'b')
@@ -180,52 +95,9 @@ dot_product(const vsvec<_T, _A>& a, int32_t stride_a, size_t offset_a,
              "int32_t stride_b, size_t offset_b)";
         throw std::domain_error(s.str());
     }
-    const size_t s= a.size()/stride_a;
-    const size_t n1= s & ~(_N-1);
-    _T r(0);
-    if (n1) {
-        using v_t = vec<_T, _N>;
-        using vi_t = vec<int32_t, _N>;
-        const size_t n4= s & ~(_N4-1);
-        vec<_T, _N> r0(_T(0));
-        vi_t idx_a=
-            impl::indices_from_stride<vi_t, int32_t>::create(stride_a);
-        vi_t idx_b=
-            impl::indices_from_stride<vi_t, int32_t>::create(stride_b);
-        auto lck_a=make_variable_lookup_table<_T>(idx_a);
-        auto lck_b=make_variable_lookup_table<_T>(idx_b);
-        if (n4) {
-            vec<_T, _N> r1(_T(0)), r2(_T(0)), r3(_T(0));
-            for (size_t i=0; i<n4; i+=4*_N) {
-                v_t a0 = lck_a.fromp(&a[(i+0*_N)*stride_a+offset_a]);
-                v_t a1 = lck_a.fromp(&a[(i+1*_N)*stride_a+offset_a]);
-                v_t a2 = lck_a.fromp(&a[(i+2*_N)*stride_a+offset_a]);
-                v_t a3 = lck_a.fromp(&a[(i+3*_N)*stride_a+offset_a]);
-                v_t b0 = lck_b.fromp(&b[(i+0*_N)*stride_b+offset_b]);
-                v_t b1 = lck_b.fromp(&b[(i+1*_N)*stride_b+offset_b]);
-                v_t b2 = lck_b.fromp(&b[(i+2*_N)*stride_b+offset_b]);
-                v_t b3 = lck_b.fromp(&b[(i+3*_N)*stride_b+offset_b]);
-
-                r0 += a0 * b0;
-                r1 += a1 * b1;
-                r2 += a2 * b2;
-                r3 += a3 * b3;
-            }
-            r2 += r3;
-            r0 += r1;
-            r0 += r2;
-        }
-        for (size_t i=n4; i<n1; i+= _N) {
-            v_t a0 = lck_a.fromp(&a[(i+0*_N)*stride_a+offset_a]);
-            v_t b0 = lck_b.fromp(&b[(i+0*_N)*stride_b+offset_b]);
-            r0 += a0 * b0;
-        }
-        r = hadd(r0);
-    }
-    for (size_t i=n1; i<s; ++i) {
-        r += a[i*stride_a+offset_a] * b[i*stride_b+offset_b];
-    }
-    return r;
+    return impl::dot_product<_T, _T>(a.size()/stride_a,
+                                     a.cbegin(), stride_a, offset_a,
+                                     b.cbegin(), stride_b, offset_b);
 }
 
 template <typename _A>
