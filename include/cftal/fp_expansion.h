a68ad7d9cd556e3acba7be5682fb75da0a3e5d38
axze <az0123456@gmx.de>
2026-04-26 17:33:02 +0200

    normalization is required by comparison operators
    the comparison operators require the normalization of the
    fp_expansions compared

--- include/cftal/fp_expansion.h
+++ include/cftal/fp_expansion.h
@@ -64,42 +64,48 @@ namespace cftal {
     std::ostream&
     operator<<(std::ostream& s, const fp_expansion<_T, _N>& e);
 
-    // less than operator for fp_expansions
+    // less than operator for fp_expansions, requires normalization of
+    // both fp_expansions
     template <typename _T, size_t _N>
     auto
     operator<(const fp_expansion<_T, _N>& a,
               const fp_expansion<_T, _N>& b)
         ->decltype(_T{} < _T{});
 
-    // less than or equal operator for fp_expansions
+    // less than or equal operator for fp_expansions, requires
+    // normalization of both fp_expansions
     template <typename _T, size_t _N>
     auto
     operator<=(const fp_expansion<_T, _N>& a,
                const fp_expansion<_T, _N>& b)
         ->decltype(_T{}<=_T{});
 
-    // equality operator for fp_expansions
+    // equality operator for fp_expansions, requires normalization of
+    // both fp_expansions
     template <typename _T, size_t _N>
     auto
     operator==(const fp_expansion<_T, _N>& a,
                const fp_expansion<_T, _N>& b)
         ->decltype(_T{}==_T{});
 
-    // non equality operator for fp_expansions
+    // non equality operator for fp_expansions, requires normalization
+    // of both fp_expansions
     template <typename _T, size_t _N>
     auto
     operator!=(const fp_expansion<_T, _N>& a,
                const fp_expansion<_T, _N>& b)
         ->decltype(_T{}!=_T{});
 
-    // greater than or equal operator for fp_expansions
+    // greater than or equal operator for fp_expansions, requires
+    // normalization of both fp_expansions
     template <typename _T, size_t _N>
     auto
     operator>=(const fp_expansion<_T, _N>& a,
                const fp_expansion<_T, _N>& b)
         ->decltype(_T{}>=_T{});
 
-    // greater than operator for fp_expansions
+    // greater than operator for fp_expansions, requires normalization
+    // of both fp_expansions
     template <typename _T, size_t _N>
     auto
     operator>(const fp_expansion<_T, _N>& a,
